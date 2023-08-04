55507a2be78b375bff90ce577dfb23b5eec30693
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-08-05 01:36:36 +0300

    make sure iam in base dir

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from pprint import pprint
 from controllers.link_collector import BrowserBookmark
 
@@ -6,10 +8,14 @@ from common.utils.files import dump_to_file
 
 
 def main():
+    # make sure that iam in base dir `src`
+    if os.getcwd().endswith('src') is False:
+        os.chdir('./src')
+
     controller = BrowserBookmark('resources/bookmarks/firefox_bookmarks.html')
     bookmarks = controller.from_html()
     json_bookmarks = controller.to_json(bookmarks)
-    dump_to_file('./bookmarks.json', json_bookmarks)
+    dump_to_file('../bookmarks.json', json_bookmarks)
 
 
 if __name__ == '__main__':
