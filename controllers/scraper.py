b19ad9ead2d0133c16938345679cf63e5ed221d0
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-08-05 23:26:20 +0300

    modify the scraper to get more data we need and export them with dto

--- controllers/scraper.py
+++ controllers/scraper.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-from common.utils.dto import Bookmark
+from common.utils.dto import Bookmark, BookmarkWebPage, HTMLMetaTag
+from common.utils.dicts import get_by_alias
 
 
 class BrowserBookmarkScraper:
@@ -10,10 +11,25 @@ class BrowserBookmarkScraper:
 
     def pull(self):
         print(f'[pull] {self.bookmark.url}')
-        res = requests.get(self.bookmark.url)
+        res = requests.get(self.bookmark.url, timeout=3)
         assert res.status_code == 200, f'Error request [{res.status_code}]'
 
         soup = BeautifulSoup(res.text, 'lxml')
-        meta_tags = soup.select('meta')
-        for meta in meta_tags:
-            print(meta)
+
+        web_page = BookmarkWebPage(
+            id=self.bookmark.id,
+            url=self.bookmark.url,
+            title=soup.select_one('title').text
+        )
+        meta_tags = []
+        for elm in soup.select('meta[content]'):
+            name = get_by_alias(
+                elm.attrs, ['name', 'property', 'itemprop'], 'undefined')
+            meta_tag = HTMLMetaTag(
+                name=name,
+                content=elm.attrs.get('content')
+            )
+            meta_tags.append(meta_tag)
+
+        web_page.meta_tags = meta_tags
+        return web_page
