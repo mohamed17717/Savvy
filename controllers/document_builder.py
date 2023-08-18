b94e0d9036bae101f03a1798e4b96fd3fbcb2474
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-08-18 08:19:46 +0300

    create simple document builder using simple weighting method

--- controllers/document_builder.py
+++ controllers/document_builder.py
@@ -1,4 +1,39 @@
+import re
+from typing import Dict
 from common.utils.dto import Bookmark, BookmarkWebpage
+
+
+# Create WeightedDocumentBuilder
+class BookmarkWeightedDocumentBuilder:
+    def __init__(self, bookmark: Bookmark, webpage: BookmarkWebpage) -> None:
+        self.bookmark = bookmark
+        self.webpage = webpage
+
+    def build(self) -> Dict[str, int]:
+        # output is { word: weight }
+        weight_sheet = (
+            (self.webpage.url, 2),
+            (self.webpage.title, 8),
+            (self.bookmark.title, 5),
+            (self.webpage.meta_data, 3),
+            (self.bookmark.domain, 4),
+        )
+
+        weighted_words = {}
+        for phrase, weight in weight_sheet:
+            # remove punctuation
+            phrase = re.sub(r'[^\w ]', ' ', phrase)
+            # remove double spaces
+            phrase = re.sub(r' {2,}', '', phrase)
+
+            words = phrase.split(' ')
+            for word in words:
+                if weighted_words.get(word) is not None:
+                    weighted_words[word] += weight
+                else:
+                    weighted_words[word] = weight
+
+        return weighted_words
 
 
 class BookmarkDocumentBuilder:
