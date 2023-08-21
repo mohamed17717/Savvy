d1a0503b2a477513018ffb2bdc9077b7f1382ad6
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-08-21 14:13:20 +0300

    run the code

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def main():
     # tests.test_collector_from_html()
     # tests.test_collector_to_json()
     # tests.test_scraper()
-    tests.test_document_builder()
+    # tests.test_document_builder()
+    # tests.test_clean_documents()
+    from controllers import classifier
 
 if __name__ == '__main__':
     main()
