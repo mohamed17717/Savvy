c663c97c7c867079dec46a771706a524c28e84cf
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-08-22 07:08:43 +0300

    adv text clean

--- common/utils/string.py
+++ common/utils/string.py
@@ -2,6 +2,12 @@ import re
 import string
 import random
 
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+import spacy
+import arabicstopwords.arabicstopwords as stp
+
 
 def random_string() -> str:
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
@@ -47,5 +53,35 @@ def clean_string(string: str) -> str:
 
     return tmp.strip()
 
-def adv_clean_string(string: str):
-    ...
+
+def adv_clean_string(string: str, stem: str = 'None'):
+    # Remove stop words
+    text = text.split()
+    useless_words = nltk.corpus.stopwords.words("english")
+    # arabic_words = nltk.corpus.stopwords.words("arabic")
+    # arabic_words = stp.stopwords_list()
+    useless_words = useless_words + ['hi', 'im']
+
+    text_filtered = [word for word in text if not word in useless_words]
+
+    # Stem or Lemmatize
+    if stem == 'Stem':
+        stemmer = PorterStemmer()
+        text_stemmed = [stemmer.stem(y) for y in text_filtered]
+    elif stem == 'Lem':
+        lem = WordNetLemmatizer()
+        text_stemmed = [lem.lemmatize(y) for y in text_filtered]
+    elif stem == 'Spacy':
+        # This line should loaded outside
+        nlp = spacy.load('en_core_web_sm')
+
+        text_filtered = nlp(' '.join(text_filtered))
+        text_stemmed = [y.lemma_ for y in text_filtered]
+    else:
+        text_stemmed = text_filtered
+
+    final_string = ' '.join(text_stemmed)
+
+    # for more steps check this
+    # https://monkeylearn.com/blog/text-cleaning/
+    return final_string
