a70a917d506c35ec83bef1c24b9c2211d068268c
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-08-27 11:42:20 +0300

    run the code

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import os
 
 import tests
 
+
 def main():
     if os.getcwd().endswith('src') is False:
         os.chdir('./src')
-    
+
     # tests.test_collector_from_html()
     # tests.test_collector_to_json()
     # tests.test_scraper()
     # tests.test_document_builder()
     # tests.test_clean_documents()
-    from controllers import document_cluster
+    # tests.test_cluster_documents()
+    tests.test_label_clusters()
+
 
 if __name__ == '__main__':
     main()
