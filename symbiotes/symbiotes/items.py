b1f52364e0c330fc98615e917fc477a91cae8568
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-09-16 14:54:10 +0300

    add items for more data processing before saving it

--- symbiotes/symbiotes/items.py
+++ symbiotes/symbiotes/items.py
@@ -4,9 +4,41 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
 
 
 class SymbiotesItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class BookmarkItem(scrapy.Item):
+    meta_tags = scrapy.Field()
+    headers = scrapy.Field()
+    url = scrapy.Field()
+    page_title = scrapy.Field()
+
+
+class BookmarkItemLoader(ItemLoader):
+    default_item_class = BookmarkItem  # Specify the Item class
+
+    def strip_whitespace_from_dict(self, values):
+        def clean_headers(headers: dict):
+            # structure is {h1: [text1, text2], h2: [text1, text2]}
+            cleaned_headers = {}
+            for tag_name, texts in headers.items():
+                texts = [text.strip() for text in texts]
+                texts = filter(lambda i: i, texts)
+                texts = list(texts)
+                if texts:
+                    cleaned_headers[tag_name] = texts
+            return cleaned_headers
+
+        values = map(clean_headers, values)
+        values = filter(lambda i: i, values)
+        values = list(values)
+
+        return values
+
+    headers_out = strip_whitespace_from_dict
