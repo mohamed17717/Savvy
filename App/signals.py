5d4b2a35e5d12c76ece09b89710f0923c43aa01d
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-09-25 04:59:32 +0300

    create triggers after create the bookmark file

--- App/signals.py
+++ App/signals.py
@@ -1,36 +1,30 @@
-import json
-
 from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
+
+from scrapy.crawler import CrawlerProcess
+from symbiotes.symbiotes.spiders.venom import VenomSpider
 
 from App import models
 
 
 @receiver(post_save, sender=models.BookmarkFile)
-def on_create_bookmark_file_extract_urls(sender, instance:models.BookmarkFile, created, **kwargs):
-    if created is False: return
+def on_create_bookmark_file_extract_urls(sender, instance: models.BookmarkFile, created, **kwargs):
+    if created is False:
+        return
 
-    # NOTE: THIS IS PSEUDO CODE
-    src = instance.file_content
-    if instance.is_html:
-        urls = []
-    elif instance.is_json:
-        urls = json.loads(src)
+    bookmarks = [
+        models.Bookmark.instance_by_parent(instance, bookmark)
+        for bookmark in instance.bookmarks
+    ]
+    models.Bookmark.objects.bulk_create(bookmarks)
 
-    for url in urls:
-        models.Bookmark(url=url)
-    
-    # >>> give command to scrapy to start the spider
-    # there is new urls come here
+    # TODO: give command to scrapy to start the spider
     # also run them in background celery task
-
+    # process = CrawlerProcess()
+    # process.crawl(VenomSpider, urls)
+    # process.start()
 
 
 @receiver(pre_save, sender=models.BookmarkFile)
-def on_save_bookmark_file_validate_file_content(sender, instance:models.BookmarkFile, created, **kwargs):
-    if instance.is_html:
-        # validate it contain urls and is a real bookmark file
-        ...
-    elif instance.is_json:
-        # is an array of urls
-        ...
+def on_save_bookmark_file_validate_file_content(sender, instance: models.BookmarkFile, created, **kwargs):
+    instance.file_obj.validate(raise_exception=True)
