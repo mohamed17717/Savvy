bd00e96c5ba9d4f4d5d56f12f22b93ad303cbec8
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-09-28 01:02:12 +0300

    run scrapy after pulling the urls

--- App/signals.py
+++ App/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 
 from scrapy.crawler import CrawlerProcess
-from symbiotes.symbiotes.spiders.venom import VenomSpider
+from crawler.spiders.venom import VenomSpider
 
 from App import models
 
@@ -18,11 +18,11 @@ def on_create_bookmark_file_extract_urls(sender, instance: models.BookmarkFile, 
     ]
     models.Bookmark.objects.bulk_create(bookmarks)
 
-    # TODO: give command to scrapy to start the spider
-    # also run them in background celery task
-    # process = CrawlerProcess()
-    # process.crawl(VenomSpider, urls)
-    # process.start()
+    # run them in background celery task
+    urls = [bookmark['url'] for bookmark in instance.bookmarks]
+    process = CrawlerProcess()
+    process.crawl(VenomSpider, urls)
+    process.start()
 
 
 @receiver(pre_save, sender=models.BookmarkFile)
