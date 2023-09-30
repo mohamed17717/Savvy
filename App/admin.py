5daef42c1f543502bb8472fec5e8208f752d0281
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-09-30 03:40:47 +0300

    register models to admin dashboard

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from App import models
+
+
+admin.site.register(models.BookmarkFile)
+admin.site.register(models.Bookmark)
+admin.site.register(models.ScrapyResponseLog)
+admin.site.register(models.BookmarkWebpage)
+admin.site.register(models.WebpageMetaTag)
+admin.site.register(models.WebpageHeader)
+admin.site.register(models.DocumentWordWeight)
+admin.site.register(models.DocumentCluster)
+admin.site.register(models.ClusterTag)
