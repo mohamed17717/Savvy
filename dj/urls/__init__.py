d4f414da317e53385b7f0a91e34d305d1ff8f18e
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-07 07:38:43 +0300

    swagger and urls

---
+++ dj/urls/__init__.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from .swagger import swagger_urls
+
+urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += swagger_urls
