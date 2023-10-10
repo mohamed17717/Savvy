54dba0de5a08207b084d2c76cfc6d3f8e4eaa4c3
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-10 08:57:42 +0300

    urls for login system

---
+++ Users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from knox import views as knox_views
+
+from Users import views
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.LoginView.as_view(), name='knox_login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
+    path('reset-password/', views.ResetPasswordView.as_view(), name='reset_password'),
+    
+    # TODO Ask for OTP url
+    # TODO Register url
+]
