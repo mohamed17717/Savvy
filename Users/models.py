b3c068dfd9f2dac7bb8f94f7482b616242a7ba27
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-10 08:52:25 +0300

    create custom User model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from rest_framework.exceptions import ValidationError
+
+from .tasks import send_email
+
+
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    email_verified = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    def send_email(self, subject, message, confirmed_only=False):
+        if confirmed_only and self.email_verified is False:
+            raise ValidationError('Email is not verified.')
+
+        send_email.delay(subject, message, [self.email])
