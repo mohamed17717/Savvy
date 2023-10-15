4fca442ccb15f04982e64618ad8435b156e4a1d8
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-15 19:59:40 +0300

    custom exception class / add is authenticated class as default for permissions

--- dj/settings/rest_settings.py
+++ dj/settings/rest_settings.py
@@ -2,6 +2,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': (
         # make sure user who make action is authenticated and authorized for it
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.DjangoModelPermissions',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
@@ -13,4 +14,5 @@ REST_FRAMEWORK = {
         'drf_excel.renderers.XLSXRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'EXCEPTION_HANDLER': 'common.drf.exceptions.custom_exception_handler',
 }
