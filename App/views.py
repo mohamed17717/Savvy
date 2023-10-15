a8a191be162a53174508c0b140b25d94cdd3dd07
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-15 23:59:18 +0300

    create endpoint to upload bookmark file

--- App/views.py
+++ App/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.db import transaction
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.conf import settings
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+
+from App import serializers, models
+
+from Users.serializers import UserSerializer
+
+from common.utils.drf.viewsets import CRUDLViewSet, CRDLViewSet
+
+
+class BookmarkFileUploadAPI(CRDLViewSet):
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = serializers.BookmarkFileSerializer
+    queryset = models.BookmarkFile.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
