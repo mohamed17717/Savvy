b59be41d60e2e8ae958aa903e807af7a050c31b5
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-16 10:25:56 +0300

    delete unused code

--- common/utils/list_utils.py
+++
@@ -1,8 +0,0 @@
-def list_counter(ls, step=1) -> dict:
-    counter = {}
-    for i in ls:
-        if counter.get(i) is None:
-            counter[i] = step
-        else:
-            counter[i] += step
-    return counter
