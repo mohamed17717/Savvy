68013b4c35a3ef515bc7d9dddec8a5ec277ec9fb
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-22 09:58:54 +0300

    create django command that crawl bookmarks to call it later in its own process , because scrapy don't restart in same process twice

---
+++ App/management/commands/crawl_bookmarks.py
@@ -0,0 +1,29 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from crawler.spiders.bookmark import BookmarkSpider
+
+from App import models
+
+
+class Command(BaseCommand):
+    help = 'Crawl bookmarks using Scrapy'
+
+    def add_arguments(self, parser):
+        parser.add_argument('bookmarks', type=json.loads, help='List of IDs of bookmarks to scrape')
+        
+        # Options
+        parser.add_argument('--urls', nargs='+', type=str, help='URLs to scrape')
+
+    def handle(self, *args, **kwargs):
+        bookmarks = models.Bookmark.objects.filter(pk__in=kwargs['bookmarks'])
+        if bookmarks.exists() is False:
+            return
+
+        process = CrawlerProcess(settings=get_project_settings())
+        process.crawl(BookmarkSpider, list(bookmarks))
+        process.start()
