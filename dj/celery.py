3cea20f9b9f5be4974a19591b828df10d8d38427
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-24 00:54:24 +0300

    add queues and heart beat for celery config

--- dj/celery.py
+++ dj/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from datetime import timedelta
+from kombu import Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj.settings')
 
@@ -10,6 +11,15 @@ app = Celery('dj')
 
 app.conf.enable_utc = False
 app.conf.broker_connection_retry_on_startup = True
+
+app.conf.tasks_queues = (
+    Queue('default', exchange='default', routing_key='default'),
+    Queue('scrapy', exchange='scrapy', routing_key='scrapy'),
+    Queue('email', exchange='email', routing_key='email'),
+)
+app.conf.update(
+    worker_heartbeat=120,  # Send a heartbeat every 120 seconds
+)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
