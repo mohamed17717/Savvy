fa1c811fd2f20a0d7c0dee7866d39fb9df3d987d
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-25 12:13:33 +0300

    get becnhmark command

--- resources/scripts/commands.sh
+++ resources/scripts/commands.sh
@@ -5,3 +5,6 @@ celery -A dj worker -l INFO -n email_worker -Q email -E --concurrency=4
 
 celery -A dj beat -l INFO
 
+autocannon -c 100 -d 5 -p 10 -H Authorization="Token 54da3defde81841587f99f04e3d6e7b2e56c69e077e9d4f5bcb853d551763935"  http://127.0.0.1:8000/bm/bookmark/list/
+# django.db.utils.OperationalError: connection to server at "127.0.0.1", port 5432 failed: FATAL:  sorry, too many clients already
+# connection to server at "127.0.0.1", port 5432 failed: FATAL:  sorry, too many clients already
