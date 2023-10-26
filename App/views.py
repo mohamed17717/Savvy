bfd6eaee0d0c4e2da83199525ec3a9edce299749
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-26 05:01:19 +0300

    optimize some endpoint

--- App/views.py
+++ App/views.py
@@ -18,13 +18,26 @@ class BookmarkFileAPI(CRDLViewSet):
 class ClusterAPI(RLViewSet):
     # TODO cluster should be for the user only
     serializer_class = serializers.DocumentClusterDetailsSerializer
-    queryset = models.DocumentCluster.objects.all()
+    queryset = models.DocumentCluster.objects.all().prefetch_related('tags', 'bookmarks')
 
 
 class BookmarkAPI(RLViewSet):
     # TODO qs should be for the user only
     serializer_class = serializers.BookmarkSerializer
-    queryset = models.Bookmark.objects.all()
+
+    def get_queryset(self):
+        qs = models.Bookmark.objects.all()
+        if self.action == 'retrieve':
+            related_fields = ['parent_file']
+            prefetch_fields = [
+                'scrapes', 'webpages', 'clusters', 'words_weights',
+                'webpages__meta_tags', 'webpages__headers',
+                'clusters__tags'
+            ]
+            qs = qs.prefetch_related(
+                *prefetch_fields).select_related(*related_fields)
+
+        return qs
 
     def retrieve(self, request, *args, **kwargs):
         self.serializer_class = serializers.BookmarkDetailsSerializer
