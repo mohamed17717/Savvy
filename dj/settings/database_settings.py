93727564634a7d4eae442f05bbba672e1b722595
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-26 03:53:55 +0300

    solve issue -> when make benchmark with 1000 concurrent request db brefuse to connect so i changed the engine to psycopg2 and give max age and atomic requests

--- dj/settings/database_settings.py
+++ dj/settings/database_settings.py
@@ -3,11 +3,14 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv("POSTGRES_DB"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
         'PORT': os.getenv("POSTGRES_PORT", '5432'),
+        'CONN_MAX_AGE': 600,
+        'ATOMIC_REQUESTS': True,
     }
 }
