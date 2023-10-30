edb18252be4fc733ac32efd3e08aede99e9ec8ee
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-10-30 09:31:26 +0200

    setup email provider

--- dj/settings/__init__.py
+++ dj/settings/__init__.py
@@ -14,6 +14,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 from .app_settings import *
 from .database_settings import *
 from .localize_settings import *
