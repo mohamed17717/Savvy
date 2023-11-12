1f595d8d6b60d14972ae4b3d9cd6e5fefcbcc082
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-12 05:58:48 +0200

    create new celery signal which add new state for the task to differntiate between real task and fake one /// also make the crawl bookmark task adabt with the change of accepting a list of ids

--- App/tasks.py
+++ App/tasks.py
@@ -2,12 +2,24 @@ import json
 import subprocess
 
 from celery import shared_task
+from celery import current_app
+from celery.signals import after_task_publish
+
+
+@after_task_publish.connect
+def update_sent_state(sender=None, headers=None, **kwargs):
+    # the task may not exist if sent using `send_task` which
+    # sends tasks by name, so fall back to the default result backend
+    # if that is the case.
+    task = current_app.tasks.get(sender)
+    backend = task.backend if task else current_app.backend
+ 
+    backend.store_result(headers['id'], None, "SENT")
 
 
 @shared_task(queue='scrapy')
-def crawl_bookmarks_task(bookmarks):
-    ids = json.dumps([bm.id for bm in bookmarks])
-    command = ['python', 'manage.py', 'crawl_bookmarks', ids]
+def crawl_bookmarks_task(bookmark_ids: list[int]):
+    command = ['python', 'manage.py', 'crawl_bookmarks', json.dumps(bookmark_ids)]
 
     try:
         result = subprocess.run(
@@ -17,3 +29,5 @@ def crawl_bookmarks_task(bookmarks):
         print("Command output:", result.stdout)
     except subprocess.CalledProcessError as e:
         print("Command failed with error:", e)
+        
+    return True
