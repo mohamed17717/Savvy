5298457fc114ae3f036e5987e691195b2b8b13c1
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-13 05:44:12 +0200

    create tests for scrapy

--- App/tests/__init__.py
+++ App/tests/__init__.py
@@ -1,5 +1,4 @@
 from .apis_tests import *
 from .controllers_tests import *
 from .models_tests import *
-from .signals_tests import *
-from .tasks_tests import *
+from .scrapy_tests import *
---
+++ App/tests/scrapy_tests.py
@@ -0,0 +1,67 @@
+import json
+from unittest import TestCase
+
+from django.conf import settings
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+
+from App import models
+from .models_tests import ObjFactory
+
+
+class BookmarkSpiderTestCase(TestCase):
+    def setUp(self) -> None:
+        print(f'\n\n{settings.DATABASES["default"]["NAME"]}\n\n')
+        self.urls = [
+            'https://dev.to/koladev/authentication-in-tests-with-drf-4jin',
+            # 'https://dev.to/devteam/what-was-your-win-this-week-30k1',
+            # 'https://dev.to/devteam/dev-community-contributor-spotlight-christine-belzie-38bg',
+            # 'https://dev.to/lucamartial/why-is-it-so-important-to-evaluate-large-language-models-llms-59jl',
+            # 'https://dev.to/encore/building-a-fully-type-safe-event-driven-backend-in-go-2g8m',
+        ]
+        self.user = ObjFactory.create_user('hello_scrapy')
+        self.bookmarks = []
+
+        for url in self.urls:
+            bm = models.Bookmark.objects.create(url=url, user=self.user)
+            self.bookmarks.append(bm)
+
+    def test_run_crawl_command(self):
+        ids = [bm.id for bm in self.bookmarks]
+        call_command('crawl_bookmarks', json.dumps(ids))
+        
+        # bookmark crawled status is changes
+        # self.assertGreaterEqual(
+        #     bookmark_file.bookmarks.filter(crawled=True).count(), 1
+        # )
+
+        # scrapy log created
+        scrapes = models.ScrapyResponseLog.objects.filter(bookmark__in=self.bookmarks)
+        self.assertGreaterEqual(scrapes.count(), len(self.urls))
+
+        # bookmarks webpage header and meta created
+        webpages = models.BookmarkWebpage.objects.filter(bookmark__in=self.bookmarks)
+        self.assertGreaterEqual(webpages.count(), len(self.urls))
+
+        meta_tags = models.WebpageMetaTag.objects.filter(webpage__in=webpages)
+        self.assertGreaterEqual(meta_tags.count(), len(self.urls))
+
+        headers = models.WebpageHeader.objects.filter(webpage__in=webpages)
+        self.assertGreaterEqual(headers.count(), len(self.urls))
+
+        # bookmarks have word vector
+        words = models.DocumentWordWeight.objects.filter(document__in=self.bookmarks)
+        self.assertGreaterEqual(words.count(), len(self.urls))
+
+        # bookmarks got clustered
+        clusters = models.DocumentCluster.objects.filter(bookmarks__in=self.bookmarks)
+        self.assertGreaterEqual(clusters.count(), 1)
+
+        # clustered got labels
+        # cluster_labels = models.ClusterTag.objects.filter(cluster__in=clusters)
+        # self.assertGreaterEqual(cluster_labels.count(), 1)
+
+    def tearDown(self) -> None:
+        ids = [bm.id for bm in self.bookmarks]
+        models.Bookmark.objects.filter(user=self.user, id__in=ids).delete()
+        get_user_model().objects.filter(id=self.user.id).delete()
