fc1316809b3b1ae54ca0d5268e8c1f131594f9a1
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-17 23:32:44 +0200

    celery test in different broker

--- dj/settings/celery_settings.py
+++ dj/settings/celery_settings.py
@@ -1,10 +1,14 @@
 
 # Celery SETTINGS
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_TASK_SERIALIZER = 'pickle'
 CELERY_RESULT_SERIALIZER = 'pickle'
 CELERY_TIMEZONE = 'UTC'
 CELERY_ACCEPT_CONTENT = ['pickle']
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 12 * 60 * 60}
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+
+if 'test' in sys.argv:
+    CELERY_BROKER_URL = 'redis://localhost:6379/1'
