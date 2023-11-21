a06192c08de5ccaea58601e8e3517f036649b494
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-21 08:05:13 +0200

    user promethious controller to log cache hits

--- dj/settings/cache_settings.py
+++ dj/settings/cache_settings.py
@@ -1,7 +1,8 @@
 # Caches
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
+        # "BACKEND": "django_redis.cache.RedisCache",
+        "BACKEND": "django_prometheus.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
