ee1384e6b94f11556d4803c3d6e786d1647b17a5
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-24 17:43:03 +0200

    new method on cluster to refresh labels

--- App/models/clusters_models.py
+++ App/models/clusters_models.py
@@ -26,6 +26,25 @@ class DocumentCluster(models.Model):
 
         return general_vector
 
+    def refresh_labels(self):
+        general_vector = self.general_words_vector
+
+        # sort words desc on its general weight
+        words = sorted(general_vector.keys(), key=lambda i: -general_vector[i])
+
+        # 2/3 of words consider tops but less than 10
+        MAX_WORDS_COUNT = 10
+        top_words_count = len(words) * 2 // 3
+        top_words_count = min(MAX_WORDS_COUNT, top_words_count)
+        top_words = words[:top_words_count]
+
+        # store labels
+        # TODO words with be duplicated and this is not efficient relation
+        # solve it
+        ClusterTag.objects.bulk_create([
+            ClusterTag(cluster=self, name=word) for word in top_words
+        ])
+
 
 class ClusterTag(models.Model):
     # Relations
