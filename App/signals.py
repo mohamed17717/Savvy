15821027573babc23c7e7aaeaf8cbd90b4b0f8ae
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-24 17:42:26 +0200

    remove creating cluster tags signal because it depend on bookmarks and when signal fired it don't have any bookmark

--- App/signals.py
+++ App/signals.py
@@ -26,26 +26,3 @@ def on_create_bookmark_file_extract_urls(sender, instance, created, **kwargs):
 @receiver(pre_save, sender=models.BookmarkFile)
 def on_save_bookmark_file_validate_file_content(sender, instance, **kwargs):
     instance.file_obj.validate(raise_exception=True)
-
-
-@receiver(post_save, sender=models.DocumentCluster)
-def on_create_cluster_start_labeling(sender, instance, created, **kwargs):
-    if not created:
-        return
-
-    general_vector = instance.general_words_vector
-
-    # sort words desc on its general weight
-    words = sorted(general_vector.keys(), key=lambda i: -general_vector[i])
-
-    # 2/3 of words consider tops but less than 10
-    MAX_WORDS_COUNT = 10
-    top_words_count = len(words) * 2 // 3
-    top_words_count = min(MAX_WORDS_COUNT, top_words_count)
-    top_words = words[:top_words_count]
-
-    # store labels
-    models.ClusterTag.objects.bulk_create([
-        models.ClusterTag(cluster=instance, name=word)
-        for word in top_words
-    ])
