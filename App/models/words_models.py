d46f1c29815b2924b8a8cb7f415819a6d3d5ee54
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-28 16:21:54 +0200

    rename field to bm // use custom manager to make sure signals fired at bulk create

--- App/models/words_models.py
+++ App/models/words_models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from App.managers import DocumentWordWeightManager
 
 User = get_user_model()
-
 
 
 class DocumentWordWeight(models.Model):
     # Relations
     # TODO in future -> this field become generic relation
     # to relate with (bookmark / youtube / linkedin / etc...)
-    document = models.ForeignKey(
+    bookmark = models.ForeignKey(
         'App.Bookmark', on_delete=models.CASCADE, related_name='words_weights')
 
     # Required
@@ -22,3 +22,7 @@ class DocumentWordWeight(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    objects = DocumentWordWeightManager()
+
+    def __str__(self):
+        return f'{self.word} - {self.weight} <DOC: {self.bookmark}>'
