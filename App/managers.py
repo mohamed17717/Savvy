7246e81d0accffb3c8058de52a1407cbb5765070
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-28 16:45:57 +0200

    add same functionality for create

--- App/managers.py
+++ App/managers.py
@@ -1,16 +1,36 @@
+from functools import partial
+from typing import Any
+
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 
 class SignalsCustomManager(models.Manager):
     '''
     Custom manager to fire signals on create and bulk create
     '''
+
+    def _trigger(self, objs):
+        if not objs: return
+
+        if not isinstance(objs, list):
+            objs = [objs]
+
+        signals = {
+            'post_save': partial(post_save.send, sender=self.model, created=True),
+            'pre_save': partial(pre_save.send, sender=self.model),
+        }
+
+        for signal in signals.values():
+            for i in objs:
+                signal(instance=i)
+
+    def create(self, **kwargs: Any) -> Any:
+        obj = super().create(**kwargs)
+        self._trigger(obj)
+        return obj
+
     def bulk_create(self, objs, **kwargs):
-        results = super().bulk_create(objs, **kwargs)
-
-        # to make sure signals are saved
-        for i in objs:
-            post_save.send(i.__class__, instance=i, created=True)
-
-        return results
+        result = super().bulk_create(objs, **kwargs)
+        self._trigger(objs)
+        return result
