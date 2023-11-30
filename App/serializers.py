1a0f48a747b05c4fb19210027d125f6574d151d9
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-11-30 02:49:53 +0200

    create serializer that handle weighting for the bookmark

--- App/serializers.py
+++ App/serializers.py
@@ -1,6 +1,8 @@
+import urllib3
+from typing import Dict
 from rest_framework import serializers
 
-from App import models
+from App import models, controllers
 
 
 class BookmarkFileSerializer(serializers.ModelSerializer):
@@ -112,3 +114,132 @@ class DocumentClusterDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.DocumentCluster
         fields = '__all__'
+
+
+class BookmarkWeightingSerializer(serializers.ModelSerializer):
+    '''This serializer is used for access, clean and weight bookmark data
+    return a vectors of weights for each type
+    '''
+    url = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
+    domain = serializers.SerializerMethodField()
+    site_name = serializers.SerializerMethodField()
+    webpage_headers = serializers.SerializerMethodField()
+    webpage_meta_data = serializers.SerializerMethodField()
+
+    def __weight(self, text, weight_factor) -> Dict[str, int]:
+        weights = {}
+        for word in text.split(' '):
+            weights.setdefault(word, 0)
+            weights[word] += weight_factor
+        return weights
+
+    def __merge_weights(self, weight1, weight2) -> Dict[str, int]:
+        merged = weight1.copy()
+        for word, weight in weight2.items():
+            merged.setdefault(word, 0)
+            merged[word] += weight
+        return merged
+
+    def __clean_text(self, text) -> str:
+        cleaned = (controllers.TextCleaner(text)
+            .html_entities()
+            .html_tags()
+            .emails()
+            .usernames()
+            .links()
+            .hashtags()
+            .repeating_chars()
+            .lines()
+            .not_letters()
+            .underscore()
+            .numbers()
+            .uncamelcase()
+            .lowercase()
+            .stop_words()
+            .shorter_than(length=2)
+            .double_spaces()
+        ).text
+        return cleaned
+
+    def __clean_url(self, url) -> str:
+        path = urllib3.util.parse_url(url).path
+        cleaned = (controllers.TextCleaner(path)
+            .not_letters()
+            .underscore()
+            .numbers()
+            .uncamelcase()
+            .stop_words()
+            .shorter_than(length=2)
+            .double_spaces()
+            .lowercase()
+        ).text
+        return cleaned
+
+    def get_url(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 1
+
+        url = obj.url or obj.webpage.url # TODO if urls are not the same do something
+        cleaned = self.__clean_url(url)
+
+        return self.__weight(cleaned, WEIGHT_FACTOR)
+
+    def get_title(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 1
+
+        title = obj.title or obj.webpage.title # TODO if titles are not the same do something
+        cleaned = self.__clean_text(title)
+
+        return self.__weight(cleaned, WEIGHT_FACTOR)
+    
+    def get_domain(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 1
+        domain = obj.domain
+        return self.__weight(domain, WEIGHT_FACTOR)
+
+    def get_site_name(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 1
+        site_name = obj.site_name
+        return self.__weight(site_name, WEIGHT_FACTOR)
+
+    def get_webpage_headers(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 1
+
+        headers = obj.webpage.headers.all()
+        weights = {}
+        for header in headers:
+            cleaned = self.__clean_text(header.text)
+            weight_factor = header.weight_factor * WEIGHT_FACTOR
+            header_weight = self.__weight(cleaned, weight_factor)
+
+            weights = self.__merge_weights(weights, header_weight)
+            
+        return weights
+
+    def get_webpage_meta_data(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 1
+
+        meta_tags = obj.webpage.meta_tags.all()
+        weights = {}
+        for meta in meta_tags:
+            if meta.content is None:
+                continue
+
+            cleaned = self.__clean_text(meta.content)
+            weight_factor = meta.weight_factor * WEIGHT_FACTOR
+            meta_weight = self.__weight(cleaned, weight_factor)
+
+            weights = self.__merge_weights(weights, meta_weight)
+            
+        return weights
+
+    class Meta:
+        model = models.Bookmark
+        fields = [
+            'url',
+            'title',
+            'domain',
+            'site_name',
+            'webpage_headers',
+            'webpage_meta_data',
+        ]
