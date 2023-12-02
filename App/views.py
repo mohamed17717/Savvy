80b90248f4bd1a9358cad7f387c50db41b99a95f
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-12-02 15:14:04 +0200

    create views to list/read tags and list most weighted ones and update alias name endpoint

--- App/views.py
+++ App/views.py
@@ -1,4 +1,6 @@
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.generics import UpdateAPIView, GenericAPIView
+from rest_framework.response import Response
 
 from App import serializers
 
@@ -32,3 +34,32 @@ class BookmarkAPI(RLViewSet):
     def retrieve(self, request, *args, **kwargs):
         self.serializer_class = serializers.BookmarkDetailsSerializer
         return super().retrieve(request, *args, **kwargs)
+
+
+class TagAPI(RLViewSet):
+    serializer_class = serializers.TagDetailsSerializer
+
+    def get_queryset(self):
+        return self.request.user.tags.all().prefetch_related('bookmarks')
+
+
+class TagUpdateAliasNameAPI(UpdateAPIView):
+    serializer_class = serializers.TagAliasNameSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.tags.all()
+
+
+class TagMostWeightedListAPI(GenericAPIView):
+    serializer_class = serializers.TagSerializer
+    TAGS_COUNT = 50
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.tags.all().order_by('-weight')[:self.TAGS_COUNT]
+
+    def get(self, request):
+        qs = self.get_queryset()
+        serializer = self.serializer_class(qs, many=True)
+        return Response(serializer.data)
