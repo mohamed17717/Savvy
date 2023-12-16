bb71f09292c7a37337c22497cf3e189ba3966cc2
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-12-16 08:44:41 +0200

    new testing commands

--- resources/scripts/commands.sh
+++ resources/scripts/commands.sh
@@ -13,4 +13,5 @@ autocannon -c 100 -d 5 -p 10 -H Authorization="Token 54da3defde81841587f99f04e3d
 # Testing for API
 python manage.py test --parallel auto --keepdb --settings=dj.settings.settings_test App.tests.apis_tests
 
-export DJANGO_SETTINGS_MODULE=dj.settings.settings_test && celery -A dj worker -l INFO -n scrapy_worker -Q scrapy -E --concurrency=4
+export DJANGO_SETTINGS_MODULE=dj.settings.settings_test && celery -A dj worker -l INFO -n scrapy_worker -Q scrapy -E --concurrency=2
+export DJANGO_SETTINGS_MODULE=dj.settings.settings_test && celery -A dj worker -l INFO -n orm_worker -Q orm -E --concurrency=2
