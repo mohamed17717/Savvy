e5f7947e050d03332760bd888815cdaa065d3b5d
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-12-16 06:25:05 +0200

    move the flow from the signal to tasks

--- App/signals.py
+++ App/signals.py
@@ -7,20 +7,10 @@ from App import models, tasks
 @receiver(post_save, sender=models.BookmarkFile)
 def on_create_bookmark_file_extract_urls(sender, instance, created, **kwargs):
     # TODO make location is not editable
-    if not created:
-        return
-
-    bookmarks = [
-        models.Bookmark.instance_by_parent(instance, bookmark)
-        for bookmark in instance.bookmarks_links
-    ]
-    # TODO add batch size or batch the data in the task
-    models.Bookmark.objects.bulk_create(bookmarks)
-    task = tasks.crawl_bookmarks_task.apply_async(
-        kwargs={'bookmark_ids': [bm.id for bm in bookmarks]})
-
-    instance.tasks.append(task.task_id)
-    instance.save()
+    if created:
+        tasks.store_bookmarks_task.apply_async(kwargs={
+            'parent': instance, 'bookmarks': instance.bookmarks_links
+        })
 
 
 @receiver(pre_save, sender=models.BookmarkFile)
