fc9e7e4a638ad58b220e808b881d0d31da69854e
d3v.mhmd <mohamad.mahmoud@florinz.com>
2023-12-16 06:23:13 +0200

    delete ORM models from the proxy and make the tasks instead

--- crawler/orm.py
+++ crawler/orm.py
@@ -1,5 +1,3 @@
-from django.db import transaction
-
 from asgiref.sync import sync_to_async
 
 from App import models
@@ -20,22 +18,3 @@ class DjangoProxy:
         )
         if response:
             log.store_file(response.body)
-
-    @sync_to_async
-    def webpage_write(self, bookmark, url, page_title, meta_tags, headers):
-        with transaction.atomic():
-            webpage = models.BookmarkWebpage.objects.create(
-                bookmark=bookmark, url=url, title=page_title
-            )
-
-            models.WebpageMetaTag.bulk_create(webpage, meta_tags)
-            models.WebpageHeader.bulk_create(webpage, headers)
-
-    @sync_to_async
-    def store_bookmark_weights(self, bookmark):
-        bookmark.store_word_vector()
-        bookmark.store_tags()
-
-    @sync_to_async
-    def cluster_bookmarks(self, bookmarks):
-        return models.Bookmark.cluster_bookmarks(bookmarks)
