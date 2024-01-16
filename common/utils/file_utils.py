0c0c2515bae938fb9a9f209fdcfe5c8f2be5c0be
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-16 14:38:03 +0200

    small utils to help me

---
+++ common/utils/file_utils.py
@@ -0,0 +1,9 @@
+import hashlib
+
+
+def hash_file(file_path):
+    hasher = sha256 = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hasher.update(chunk)
+    return hasher.hexdigest()
