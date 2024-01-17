7ba1e64f0cdb31afbcb5a48a744e279b78ef36bc
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-17 02:29:24 +0200

    change double quote to single quote

--- common/utils/file_utils.py
+++ common/utils/file_utils.py
@@ -4,6 +4,6 @@ from django.db import models
 
 def hash_file(file_field: models.FileField) -> str:
     hasher = hashlib.sha256()
-    for chunk in iter(lambda: file_field.read(4096), b""):
+    for chunk in iter(lambda: file_field.read(4096), b''):
         hasher.update(chunk)
     return hasher.hexdigest()
