76c6d5de5c53af2ba6735aba204db6e5dc467914
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-17 02:27:41 +0200

    catch duplicated object constraint and return valid message

--- common/utils/drf/exceptions.py
+++ common/utils/drf/exceptions.py
@@ -1,4 +1,5 @@
 from django.db.models.deletion import ProtectedError
+from django.db.utils import IntegrityError
 
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
@@ -12,6 +13,9 @@ def custom_exception_handler(exc, context):
             "error": 'Can\'t delete object have another relations in the system.',
         }
         return Response(response_data, status=status.HTTP_409_CONFLICT)
+
+    if isinstance(exc, IntegrityError) and str(exc).startswith('duplicate'):
+        return Response({"error": "This object already exists"}, status=status.HTTP_409_CONFLICT)
 
     # For other exceptions, you can use the default handler
     return exception_handler(exc, context)
