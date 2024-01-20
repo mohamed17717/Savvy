cf6a1fd13a0b536b03a74dd730e0d5b27314b3e0
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-20 19:32:32 +0200

    make sure cluster correlation works right

--- App/types/cluster_types.py
+++ App/types/cluster_types.py
@@ -1,5 +1,7 @@
-from common.utils.array import window_list
+from collections import namedtuple
 
+from common.utils.array import window_list
+from common.utils.math_utils import balanced_avg
 
 class ClustersHolderType:
     """This class type is basic 2D list with custom features
@@ -26,41 +28,44 @@ class ClustersHolderType:
         }
 
     def merge_repeated_clusters(self):
-        cluster_couples = self.ClustersCouplesType(self)
+        cluster_couples = self.ClusterCouplesHolderType(self)
 
         cluster_merge_map = {}  # x to root
         similarity_acceptance = 0.7
         for couple, times in cluster_couples.items():
             # know the length of the couple
-            short_index, long_index, short_length, long_length = (
-                cluster_couples.cluster_compare(couple))
+            _long, _short = cluster_couples.couple_details(couple)
+
             # if the repeated excel x% of the length of one of them -> then merge
-            is_very_similar = times/short_length > similarity_acceptance
+            is_very_similar = times/_short.length > similarity_acceptance
 
             if not is_very_similar:
                 continue
 
             # calculate the root
-            root = cluster_couples.graph_root(cluster_merge_map, long_index)
-            root_changed = root != long_index
+            root = cluster_couples.graph_root(cluster_merge_map, _long.index)
+            root_changed = root != _long.index
 
             # if root changed the new root should have relation to this short_index
-            if root_changed and not cluster_couples.check_couple_has_relation(short_index, root):
+            if root_changed and not cluster_couples.check_couple_has_relation(_short.index, root):
                 continue
 
             # roots can't be normal nodes so check and replace shorts to its root
             cluster_merge_map = {
-                key: (root if val == short_index else val)
+                key: (root if val == _short.index else val)
                 for key, val in cluster_merge_map.items()
             }
-            cluster_merge_map.update({short_index: root})
+            cluster_merge_map.update({_short.index: root})
 
         for point, root in sorted(cluster_merge_map.items(), key=lambda i: -i[0]):
+            root_cluster = self.value[root]
+            point_cluster = self.value[point]
+            self.correlation[root] = balanced_avg(len(root_cluster), self.correlation[root], len(point_cluster), self.correlation[point])
             self.value[root].extend(self.value.pop(point))
 
     def append(self, cluster, correlation=0, algorithm=None) -> None:
         self.value.append(
-            self.ClusterListWrapper(cluster, self, self.index)
+            self.ClusterType(cluster, self, self.index)
         )
         for item in cluster:
             self.items_map.setdefault(item, [])
@@ -74,8 +79,7 @@ class ClustersHolderType:
     def bulk_append(self, *clusters, correlation=0, algorithm=None):
         if clusters:
             for cluster in clusters:
-                self.append(
-                    cluster, correlation=correlation, algorithm=algorithm)
+                self.append(cluster, correlation, algorithm)
 
     def __getitem__(self, index):
         return self.value[index]
@@ -83,7 +87,7 @@ class ClustersHolderType:
     def __len__(self):
         return self.index
 
-    class ClusterListWrapper(list):
+    class ClusterType(list):
         """ChildList type its a simple list with custom features
         1- know its parent
         2- know its index in its parent
@@ -96,13 +100,9 @@ class ClustersHolderType:
             self.index = index
 
         def __avg_correlation(self, new_correlation):
-            self.parent.correlation.setdefault(self.index, 1)
-            current_correlation = self.parent.correlation[self.index]
-
-            def eqn(l1, c1, l2, c2):
-                return (l1*c1 + l2*c2)/(l1+l2)
-
-            return eqn(len(self), current_correlation, 1, new_correlation)
+            correlation = self.parent.correlation.get(self.index, 1)
+            avg = balanced_avg(len(self),correlation, 1, new_correlation)
+            return avg
 
         def append(self, obj, correlation=0, algorithm=None):
             if self.parent:
@@ -115,7 +115,7 @@ class ClustersHolderType:
 
             return super().append(obj)
 
-    class ClustersCouplesType(dict):
+    class ClusterCouplesHolderType(dict):
         """
         This class aim to handle repetition in clusters and
         check the similarity between them to decide if they should
@@ -150,16 +150,16 @@ class ClustersHolderType:
                 self.__name_couple((b, a)) in self.keys()
             ])
 
-        def cluster_compare(self, couple_name):
+        def couple_details(self, couple_name):
             index1, index2 = self.__unname_couple(couple_name)
-            length1, length2 = len(self.clusters.value[index1]), len(
-                self.clusters.value[index2])
+            length1, length2 = len(self.clusters[index1]), len(self.clusters[index2])
+            
+            Item = namedtuple('Item', ['index', 'length'])
 
-            short_index, long_index, short_length, long_length = index1, index2, length1, length2
-            if length1 > length2:
-                short_index, long_index, short_length, long_length = index2, index1, length2, length1
+            result = [Item(index1, length1), Item(index2, length2)]
+            result.sort(key=lambda i: -i.length)
 
-            return short_index, long_index, short_length, long_length
+            return result
 
         def graph_root(self, graph: dict, node):
             root = node
