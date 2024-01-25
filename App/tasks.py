5ce227b9781b7be6a6b213656c7251a924897b07
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-25 04:17:04 +0200

    make sure all tasks take json as parameters instead of pickeled python objects

--- App/tasks.py
+++ App/tasks.py
@@ -1,7 +1,5 @@
 import json
-import os
 import subprocess
-from functools import partial
 
 from django.db import transaction
 
@@ -10,6 +8,7 @@ from celery.signals import after_task_publish
 from celery.result import allow_join_result
 
 from App import models
+from common.utils.array_utils import window_list
 
 
 @after_task_publish.connect
@@ -24,23 +23,22 @@ def update_sent_state(sender=None, headers=None, **kwargs):
 
 
 @shared_task(queue='orm')
-def store_bookmarks_task(parent: 'models.BookmarkFile', bookmarks: list[dict]):
-    Bookmark_Creator = partial(models.Bookmark.instance_by_parent, parent)
-    bookmarks = map(Bookmark_Creator, bookmarks)
-    bookmarks = list(bookmarks)
+def store_bookmarks_task(parent_id: int, bookmarks_data: list[dict]):
+    parent = models.BookmarkFile.objects.get(id=parent_id)
+
+    bookmarks = [parent.init_bookmark(item) for item in bookmarks_data]
+    del bookmarks_data
 
     models.Bookmark.objects.bulk_create(bookmarks, batch_size=250)
-    batch_bookmarks_to_crawl_task.apply_async(
-        kwargs={'parent': parent, 'bookmark_ids': [bm.id for bm in bookmarks]})
+    batch_bookmarks_to_crawl_task.delay([bm.id for bm in bookmarks])
 
 
 @shared_task(queue='orm', ignore_result=True)
-def batch_bookmarks_to_crawl_task(parent: 'models.BookmarkFile', bookmark_ids: list[int]):
+def batch_bookmarks_to_crawl_task(bookmark_ids: list[int]):
     batch_size = 30
-    steps = range(0, len(bookmark_ids), batch_size)
-    sliced_ids = [bookmark_ids[i:i + batch_size] for i in steps]
+    id_groups = window_list(bookmark_ids, batch_size, batch_size)
 
-    tasks = (crawl_bookmarks_task.s(ids) for ids in sliced_ids)
+    tasks = (crawl_bookmarks_task.s(group) for group in id_groups)
     callback = cluster_checker_task.s(bookmark_ids=bookmark_ids, iteration=0)
     job = chord(tasks)(callback)
     with allow_join_result():
@@ -49,24 +47,15 @@ def batch_bookmarks_to_crawl_task(parent: 'models.BookmarkFile', bookmark_ids: l
 
 @shared_task(queue='scrapy')
 def crawl_bookmarks_task(bookmark_ids: list[int]):
-    # make scrapy aware we inside a test env
-    testing_args = []
-    is_test_mode = bool(os.getenv("DJANGO_TEST_MODE"))
-    if is_test_mode:
-        testing_args = ['--settings', 'dj.settings.settings_test']
-
-    command = [
-        'python', 'manage.py', 'crawl_bookmarks',
-        json.dumps(bookmark_ids), *testing_args
-    ]
+    ids = json.dumps(bookmark_ids)
+    command = ['python', 'manage.py', 'crawl_bookmarks', ids]
     subprocess.run(command, capture_output=True, text=True, check=True)
-
-    return True
 
 
 @shared_task(queue='orm')
-def store_webpage_task(bookmark, url, page_title, meta_tags, headers):
+def store_webpage_task(bookmark_id, url, page_title, meta_tags, headers):
     with transaction.atomic():
+        bookmark = models.Bookmark.objects.get(id=bookmark_id)
         webpage = models.BookmarkWebpage.objects.create(
             bookmark=bookmark, url=url, title=page_title[:2048]
         )
@@ -76,13 +65,15 @@ def store_webpage_task(bookmark, url, page_title, meta_tags, headers):
 
 
 @shared_task(queue='orm')
-def store_weights_task(bookmark):
+def store_weights_task(bookmark_id):
+    bookmark = models.Bookmark.objects.get(id=bookmark_id)
     bookmark.store_word_vector()
     bookmark.store_tags()
 
 
 @shared_task(queue='orm')
-def cluster_bookmarks_task(bookmarks):
+def cluster_bookmarks_task(bookmark_ids):
+    bookmarks = models.Bookmark.objects.filter(id__in=bookmark_ids)
     models.Bookmark.cluster_bookmarks(bookmarks)
 
 
@@ -92,16 +83,17 @@ def cluster_checker_task(callback_result=[], bookmark_ids=[], iteration=0):
     wait_time = 10  # 10 sec
     max_iteration = max_time // wait_time
 
-    bookmarks = models.Bookmark.objects.filter(id__in=bookmark_ids)
-    uncompleted_bookmarks = bookmarks.filter(
-        crawled=True, words_weights__isnull=True).exists()
+    uncompleted_bookmarks = models.Bookmark.objects.filter(
+        id__in=bookmark_ids, crawled=True, words_weights__isnull=True
+    ).exists()
     accepted = any([
         iteration >= max_iteration,
         uncompleted_bookmarks is False
     ])
 
     if accepted:
-        cluster_bookmarks_task.apply_async(kwargs={'bookmarks': bookmarks})
+        cluster_bookmarks_task.apply_async(
+            kwargs={'bookmark_ids': bookmark_ids})
     else:
         cluster_checker_task.apply_async(
             kwargs={'bookmark_ids': bookmark_ids, 'iteration': iteration+1}, countdown=wait_time)
