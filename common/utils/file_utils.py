f2a483fa0cea1f319ff044248a1f21fe774a1461
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-26 20:21:53 +0200

    create utils for 1- random filename 2- image resize and compress 3- html utils

--- common/utils/file_utils.py
+++ common/utils/file_utils.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 import hashlib
 from django.db import models
 
@@ -7,3 +9,15 @@ def hash_file(file_field: models.FileField) -> str:
     for chunk in iter(lambda: file_field.read(4096), b''):
         hasher.update(chunk)
     return hasher.hexdigest()
+
+
+def random_filename(path):
+    def generate():
+        new_name = f"{secrets.token_hex(12)}.html"
+        return os.path.join(path, new_name)
+
+    file_path = generate()
+    while os.path.exists(file_path):
+        file_path = generate()
+
+    return file_path
---
+++ common/utils/image_utils.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from io import BytesIO
+
+
+def resize_image(content: bytes, new_width: int) -> Image:
+    image = Image.open(BytesIO(content))
+    aspect_ratio = image.height / image.width
+    new_height = int(new_width * aspect_ratio)
+
+    image = image.resize((new_width, new_height), Image.LANCZOS)
+    return image
+
+
+def compress_image(image: Image, quality: int = 85) -> bytes:
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+
+    image_io = BytesIO()
+    image.save(image_io, format='JPEG', quality=quality)
+
+    return image_io.getvalue()
