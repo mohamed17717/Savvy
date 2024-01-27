145c84461ee5d1107425b8aedba31e9895723db4
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-27 03:24:46 +0200

    register urls for tag

--- App/urls.py
+++ App/urls.py
@@ -9,12 +9,12 @@ router = CustomSuffixRouter()
 router.register(r'file', views.BookmarkFileAPI, basename='file')
 router.register(r'cluster', views.ClusterAPI, basename='cluster_read')
 router.register(r'bookmark', views.BookmarkAPI, basename='bookmark')
-router.register(r'tag', views.TagAPI, basename='tag_read')
+router.register(r'tag', views.TagAPI, basename='tag')
 
 app_name = 'app'
 
 urlpatterns = [
-    path('tag/most-weighted/', views.TagMostWeightedListAPI.as_view(), name='tag_most_weighted'),
-    path('tag/alias-name/<int:pk>/', views.TagUpdateAliasNameAPI.as_view(), name='tag_alias_name'),
+    path('tags/list/', views.TagListAPI.as_view(), name='tag_list'),
+
     path('', include(router.urls)),
 ]
