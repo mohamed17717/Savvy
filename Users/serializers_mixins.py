40d48662c12d1a4ff63d09d25921fff2b5d930b4
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-28 01:51:43 +0200

    create serializer mixin to remove duplicated code

---
+++ Users/serializers_mixins.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class ValidatePasswordMixin(object):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise ValidationError("Password and confirm password do not match")
+
+        return attrs
+
+
+class GetUserByEmailMixin(object):
+    email = serializers.CharField(required=True)
+
+    def get_user(self, email):
+        request = self.context.get('request')
+        user = request.user
+
+        if not user.is_authenticated:
+            user = User.objects.filter(email=email).first()
+            if user is None:
+                raise PermissionDenied('Not valid user data.')
+
+        return user
