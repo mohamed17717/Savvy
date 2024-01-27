9087c0e6486ee1f9a12e3ea9d18c0255bfc5e9b2
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-27 03:17:16 +0200

    math utils

--- common/utils/math_utils.py
+++ common/utils/math_utils.py
@@ -1,2 +1,10 @@
 def balanced_avg(length1, avg1, length2, avg2):
     return (avg1 * length1 + avg2 * length2) / (length1 + length2)
+
+
+def dynamic_number_boundaries(num, bottom, top):
+    if num > top:
+        num = top
+    if num < bottom:
+        num = bottom
+    return num
