887d4d6997466da952b770740c145ff8dc259192
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-27 04:06:03 +0200

    create filters for cluster model

--- App/filters.py
+++ App/filters.py
@@ -10,3 +10,13 @@ class TagFilter(filters.FilterSet):
     class Meta:
         model = models.Tag
         fields = ['name']
+
+
+class ClusterFilter(filters.FilterSet):
+    name = filters.CharFilter('name', lookup_expr='icontains')
+    correlation_min = filters.NumberFilter('correlation', lookup_expr='gte')
+    correlation_max = filters.NumberFilter('correlation', lookup_expr='lte')
+
+    class Meta:
+        model = models.DocumentCluster
+        fields = ['name', 'correlation']
