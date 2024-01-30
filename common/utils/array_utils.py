2dfa6b1128a2a065abd1c2ebc51c443831f85190
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-30 08:00:28 +0200

    solve edge case for array widnow function

--- common/utils/array_utils.py
+++ common/utils/array_utils.py
@@ -1,3 +1,6 @@
 def window_list(data: list, size: int, step: int = 1) -> list:
-    for i in range(0, len(data)-size+1, step):
+    length = len(data)
+    if length < size:
+        size = length
+    for i in range(0, length-size+1, step):
         yield data[i: i + size]
