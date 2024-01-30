38d29c153f5f20ee568519e1c34b8e3ac9cc78d4
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-01-30 08:01:08 +0200

    commands for celery

--- resources/scripts/commands.sh
+++ resources/scripts/commands.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
 
-celery -A dj worker -l INFO -n scrapy_worker -Q scrapy -E --concurrency=3
 celery -A dj worker -l INFO -n email_worker -Q email -E --concurrency=4
+celery -A dj worker -l INFO -n scrapy_worker -Q scrapy -E --concurrency=10
+celery -A dj worker -l INFO -n orm_worker -Q orm -E --concurrency=10
+
 
 celery -A dj beat -l INFO
 
