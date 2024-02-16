2efc16dc1696e32c2299c0473f6e0efa1dfe44db
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-02-16 14:14:42 +0200

    round balance avg to 3

--- common/utils/math_utils.py
+++ common/utils/math_utils.py
@@ -1,5 +1,6 @@
 def balanced_avg(length1, avg1, length2, avg2):
-    return (avg1 * length1 + avg2 * length2) / (length1 + length2)
+    avg = (avg1 * length1 + avg2 * length2) / (length1 + length2)
+    return round(avg, 3)
 
 
 def dynamic_number_boundaries(num, bottom, top):
