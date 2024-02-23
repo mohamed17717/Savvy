f43ccdf5ee8b881b07d722bdfc86226aee2e30fb
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-02-23 03:40:19 +0200

    new helper methods

--- common/utils/array_utils.py
+++ common/utils/array_utils.py
@@ -1,6 +1,13 @@
-def window_list(data: list, size: int, step: int = 1) -> list:
+from typing import Generator
+
+
+def window_list(data: list, size: int, step: int = 1) -> Generator[list, None, None]:
     length = len(data)
     if length < size:
         size = length
     for i in range(0, length-size+1, step):
         yield data[i: i + size]
+
+
+def unique_dicts_in_list(data: list[dict], key) -> list[dict]:
+    return list({d[key]: d for d in data}.values())
---
+++ common/utils/dict_utils.py
@@ -0,0 +1,4 @@
+
+
+def dict_values_to_keys(d: dict) -> dict:
+    return dict(zip(d.values(), d.keys()))
