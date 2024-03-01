71411179d665c420b5cd3084f8bc35ea0715d8ec
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-01 07:51:28 +0200

    new choices for bookmark process status

--- App/choices.py
+++ App/choices.py
@@ -18,10 +18,28 @@ class CusterAlgorithmChoices(models.IntegerChoices):
     __default__ = TRANSITIVE_SIMILARITY
 
 
-class BookmarkStatusChoices(models.IntegerChoices):
+class BookmarkUserStatusChoices(models.IntegerChoices):
     PENDING = 1, _('waiting for any status change')
     DONE = 2, _('no need to show anymore')
     ACHIEVED = 3, _('hide for now')
 
     __empty__ = None
     __default__ = PENDING
+
+
+class BookmarkProcessStatusChoices(models.IntegerChoices):
+    CLONED = 10, _('cloned from another user bookmark (to status 60)')
+    CREATED = 20, _('created')
+
+    START_CRAWL = 30, _('sent to scrapy crawler')
+    CRAWLED_ERROR = 35, _('crawled failed for any reason')
+    CRAWLED = 40, _('crawled succeeded')
+
+    START_TEXT_PROCESSING = 50, _('start text processing')
+    TEXT_PROCESSED = 60, _('text processed')
+
+    START_CLUSTER = 70, _('start clustering process')
+    CLUSTERED = 80, _('done the whole flow')
+
+    __empty__ = None
+    __default__ = CREATED
