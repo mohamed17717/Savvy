cff40847fdd9423a94eda80c3ab9c7121f2db868
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-01 09:23:08 +0200

    make sure signals fired in bulk processes

--- App/managers.py
+++ App/managers.py
@@ -1,31 +1,36 @@
-from functools import partial
-
+from typing import Any
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 
 
 class SignalsCustomManager(models.Manager):
-    '''
-    Custom manager to fire signals on create and bulk create
-    '''
+    '''Custom manager to fire signals on bulk create and update'''
 
-    def _trigger(self, objs):
-        if not objs:
-            return
+    def _trigger_pre_save(self, objs):
+        for obj in objs:
+            pre_save.send(sender=self.model, instance=obj)
 
-        if not isinstance(objs, list):
-            objs = [objs]
-
-        signals = {
-            'post_save': partial(post_save.send, sender=self.model, created=True),
-            'pre_save': partial(pre_save.send, sender=self.model),
-        }
-
-        for signal in signals.values():
-            for i in objs:
-                signal(instance=i)
+    def _trigger_post_save(self, objs, created=False):
+        for obj in objs:
+            post_save.send(sender=self.model, instance=obj, created=created)
 
     def bulk_create(self, objs, **kwargs):
+        self._trigger_pre_save(objs)
         result = super().bulk_create(objs, **kwargs)
-        self._trigger(objs)
+        self._trigger_post_save(objs, created=True)
+        return result
+
+    def bulk_update(self, objs, fields, **kwargs):
+        self._trigger_pre_save(objs)
+        result = super().bulk_update(objs, fields, **kwargs)
+        self._trigger_post_save(objs)
+        return result
+
+    def update(self, **kwargs: Any) -> int:
+        queryset = self.get_queryset()
+        objs = list(queryset)
+        self._trigger_pre_save(objs)
+        result = super().update(**kwargs)
+        self._trigger_post_save(objs)
+        
         return result
