e5e9273ebe84b84ed210e7d1487b21953b914c88
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-02 08:39:53 +0200

    remove method for duplication

--- App/tasks.py
+++ App/tasks.py
@@ -1,11 +1,9 @@
 import json
 import subprocess
 import logging
-from datetime import timedelta
 
 from django.db import transaction
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 
 from celery import shared_task, current_app, chord
 from celery.signals import after_task_publish
@@ -13,8 +11,7 @@ from celery.result import allow_join_result
 
 from App import models
 
-from common.utils.array_utils import window_list, unique_dicts_in_list
-from common.utils.dict_utils import dict_values_to_keys
+from common.utils.array_utils import window_list
 from common.utils.html_utils import extract_image_from_meta
 
 
@@ -33,35 +30,11 @@ def update_sent_state(sender=None, headers=None, **kwargs):
     backend.store_result(headers['id'], None, "SENT")
 
 
-def cleanup_duplicated_bookmarks(bookmarks: list[dict]) -> list[dict]:
-    # remove duplication from bookmarks (unique on url)
-    bookmarks = unique_dicts_in_list(bookmarks, 'url')
-    # use pre existing bookmarks
-    urls = dict(enumerate([b['url'] for b in bookmarks]))
-    urls = dict_values_to_keys(urls)
-
-    exists_bookmarks = models.Bookmark.objects.filter(
-        url__in=urls.keys(),
-        process_status__gte=models.Bookmark.ProcessStatus.TEXT_PROCESSED.value,
-        scrapes__created_at__gte=timezone.now() - timedelta(days=50),
-    )
-    # remove bookmarks from bookmarks_data
-    exists_urls = exists_bookmarks.values_list('url', flat=True)
-    indexes = sorted(map(urls.get, exists_urls), reverse=True)
-    list(map(bookmarks.pop, indexes))
-    # loop throw exists bookmarks then decide clone or skip
-    for bookmark in exists_bookmarks:
-        bookmark.deep_clone(bookmark.parent_file.user, bookmark.parent_file)
-
-    return bookmarks
-
-
 @shared_task(queue='orm')
 def store_bookmarks_task(parent_id: int, bookmarks_data: list[dict]):
     parent = models.BookmarkFile.objects.get(id=parent_id)
 
     # Start creating new bookmarks
-    bookmarks_data = cleanup_duplicated_bookmarks(bookmarks_data)
     bookmarks = tuple(map(parent.init_bookmark, bookmarks_data))
 
     models.Bookmark.objects.bulk_create(bookmarks, batch_size=250)
@@ -173,7 +146,7 @@ def cluster_checker_task(user_id, bookmark_ids=[], iteration=0):
     max_iteration = max_time // wait_time
 
     uncompleted_bookmarks = models.Bookmark.objects.filter(
-        id__in=bookmark_ids, 
+        id__in=bookmark_ids,
         process_status__gte=models.Bookmark.ProcessStatus.CRAWLED.value,
         process_status__lt=models.Bookmark.ProcessStatus.TEXT_PROCESSED.value,
     ).exists()
