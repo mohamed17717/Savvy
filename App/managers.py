d4e19eb20ea06078bcc0cac10a313b2e45ce5783
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-08 03:14:31 +0200

    - new manager for bookmark to update process status in bulk - also pub to redis on create bookmark in bulk - custom bulk signal rewrite update and bulk update methods

--- App/managers.py
+++ App/managers.py
@@ -1,9 +1,10 @@
-from typing import Any
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 
+from App import controllers
 
-class SignalsCustomManager(models.Manager):
+
+class BulkSignalsQuerySet(models.QuerySet):
     '''Custom manager to fire signals on bulk create and update'''
 
     def _trigger_pre_save(self, objs):
@@ -20,17 +21,49 @@ class SignalsCustomManager(models.Manager):
         self._trigger_post_save(objs, created=True)
         return result
 
-    def bulk_update(self, objs, fields, **kwargs):
-        self._trigger_pre_save(objs)
-        result = super().bulk_update(objs, fields, **kwargs)
-        self._trigger_post_save(objs)
-        return result
+    def bulk_update(self, objs, fields, **kwargs) -> int:
+        raise NotImplementedError(
+            'This manage not support using `.bulk_update()` instead use `.update()`')
 
-    def update(self, **kwargs: Any) -> int:
-        queryset = self.get_queryset()
-        objs = list(queryset)
+    def update(self, **kwargs) -> int:
+        objs = list(self)
+        for obj in objs:
+            for k, v in kwargs.items():
+                setattr(obj, k, v)
+
         self._trigger_pre_save(objs)
         result = super().update(**kwargs)
         self._trigger_post_save(objs)
-        
+
         return result
+
+
+class BookmarkQuerySet(models.QuerySet):
+    def bulk_create(self, objs, **kwargs):
+        result = super().bulk_create(objs, **kwargs)
+
+        if result:
+            user = result[0].user
+            publisher = controllers.BookmarkRedisPublisher(user)
+            for obj in result:
+                publisher.publish(
+                    {'bookmark_id': obj.id, 'status': obj.process_status}
+                )
+
+        return result
+
+    def update_process_status(self, new_status) -> int:
+        objs = list(self)
+        if not objs:
+            return 0
+
+        publisher = controllers.BookmarkRedisPublisher(objs[0].user)
+        for obj in objs:
+            if obj.process_status >= new_status:
+                continue
+            obj.process_status = new_status
+            publisher.publish(
+                {'bookmark_id': obj.id, 'status': new_status}
+            )
+
+        return self.bulk_update(objs, ['process_status'])
