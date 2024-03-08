98a0a5b0c980773028b990833edf625ef3461310
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-08 03:04:17 +0200

    small script for some analytics

---
+++ resources/AnalMetrics.py
@@ -0,0 +1,26 @@
+
+subscription_price = 10
+
+one_time_pay = {
+    'backend': 0,
+    'frontend': 150,
+    'company papers': 180,
+}
+
+monthly_pay_for_1000_users = {
+    'server': 5.5,
+    'proxy': 5,
+    'marketing/ads': 70,
+}
+
+pay_for_1_user = {
+    'payment gateway': 2.99/100 * subscription_price + 0.3
+}
+
+budget = ...
+real_pay = {
+    'frontend': [50],
+    'server_contabo': [11]
+}
+
+
