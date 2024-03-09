d601cf8679c4ee9969cc23a8497bd7e921a1c11d
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-09 10:49:25 +0200

    JWT manager to manage authentication between django and fastapi

---
+++ realtime/common/jwt_utils.py
@@ -0,0 +1,65 @@
+import os
+import jwt
+from datetime import datetime, timedelta
+from pydantic import BaseModel, Extra, Field
+
+from rest_framework.response import Response
+from fastapi import Request, HTTPException
+
+
+class JwtManager:
+    SECRET_KEY = os.getenv('JWT_SECRET')
+    ALGORITHM = os.getenv('JWT_ALGORITHM')
+    COOKIE_NAME = os.getenv('JWT_COOKIE_NAME')
+    ACCESS_TOKEN_EXPIRE_DAYS = 3
+
+    class AuthPayload(BaseModel, extra=Extra.allow):
+        user_id: int
+        exp: float = Field(default_factory=lambda: (
+            datetime.utcnow() + timedelta(days=JwtManager.ACCESS_TOKEN_EXPIRE_DAYS)
+        ).timestamp())
+
+    @classmethod
+    def create_access_token(cls, data: dict) -> str:
+        auth_payload = cls.AuthPayload.parse_obj(data)
+        encoded_jwt = jwt.encode(
+            auth_payload.dict(), cls.SECRET_KEY, algorithm=cls.ALGORITHM)
+        return encoded_jwt
+
+    @classmethod
+    def inject_cookie(cls, response: Response, data: dict) -> None:
+        response.set_cookie(
+            cls.COOKIE_NAME, cls.create_access_token(data),
+            httponly=True, samesite='none', secure=False
+        )
+
+    @classmethod
+    def decode_token(cls, token) -> dict:
+        data = jwt.decode(token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM])
+        if data['exp'] < datetime.utcnow().timestamp():
+            raise jwt.ExpiredSignatureError
+
+        return data
+
+    @classmethod
+    def fastapi_cookie(cls, request: Request, raise_exception=True) -> str:
+        cookie = request.cookies.get(cls.COOKIE_NAME)
+        if cookie is None and raise_exception:
+            raise jwt.InvalidTokenError
+        return cookie
+
+    @classmethod
+    def fastapi_auth(cls, request: Request) -> AuthPayload:
+        try:
+            token = cls.fastapi_cookie(request)
+            data = cls.decode_token(token)
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail="Signature expired")
+        except jwt.DecodeError:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        except Exception as e:
+            raise HTTPException(status_code=401, detail=str(e))
+
+        return cls.AuthPayload.parse_obj(data)
