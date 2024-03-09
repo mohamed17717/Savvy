a5fda074c20a154782b46bd94198edf125d1bcee
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-09 13:57:48 +0200

    start using progress controller and make sure it works fine

--- realtime/main.py
+++ realtime/main.py
@@ -3,25 +3,30 @@ import asyncio
 from fastapi import FastAPI
 
 from common.redis_utils import RedisPubSub
+from common.progress import UserProgressSingleton as UserProgress
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI()
-data = {'count': 0, 'messages': []}
+global_data = {'messages': [], 'progress': None}
 
 
 @app.get("/")
 async def index():
     print('endpoint hit')
-    return {"message": "Hello, World!", **data}
+    return {"message": "Hello, World!", **global_data}
 
 
 @app.on_event("startup")
 async def startup_event():
-    def callback(message):
-        print(f"Received message: {message}")
-        data['messages'].append(message)
-        data['count'] += 1
+    async def callback(data: dict):
+        user_id = data['user_id']
+        user_progress = await UserProgress.get_instance(user_id)
+        user_progress.change(data)
+
+        global_data['messages'].append(data)
+        global_data['progress'] = str(user_progress)
 
     asyncio.create_task(RedisPubSub.sub(callback))
