7e2d2cb657de1e6094b3d7081e24847ab59fb295
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-03-10 13:09:25 +0200

    1- add done property for user progress and calcuale it on change 2- for singleton make new method for get and get_or_create because needed in flow 3- new class for SSE progress

--- realtime/common/progress.py
+++ realtime/common/progress.py
@@ -1,6 +1,11 @@
 import json
 import asyncio
-from datetime import datetime
+import uuid
+
+from datetime import datetime, timedelta
+
+from fastapi import Request
+from sse_starlette.sse import EventSourceResponse
 
 from .redis_utils import RedisPubSub
 
@@ -20,11 +25,12 @@ class UserProgressSingleton:
         self.individual_progress = {}
         self.individual_limit = 80
         self.message = ''
+        self.DONE = False
 
     @property
-    def progress(self):
+    def progress(self) -> float:
         if self.total in [None, 0]:
-            raise Exception('Total is not set')
+            raise ValueError('Total is not set')
 
         total_progress = sum(
             self.individual_progress.values()) / self.individual_limit
@@ -48,6 +54,10 @@ class UserProgressSingleton:
         # elif data['type'] == 3:
             # TODO django should send this type
 
+        self.DONE = self.progress >= 100
+        if self.DONE:
+            self.message = 'All bookmarks are uploaded'
+
     def __str__(self):
         data = {
             'total': self.total,
@@ -60,7 +70,52 @@ class UserProgressSingleton:
     async def get_instance(cls, user_id) -> 'UserProgressSingleton':
         async with cls._lock:
             _instance = cls._instances.get(user_id)
+            return _instance
+
+    @classmethod
+    async def get_or_create_instance(cls, user_id) -> 'UserProgressSingleton':
+        async with cls._lock:
+            _instance = cls._instances.get(user_id)
             if _instance is None:
                 _instance = cls(user_id)
                 cls._instances[user_id] = _instance
             return _instance
+
+
+class ProgressSSE:
+    MESSAGE_STREAM_DELAY = 0.4  # second
+
+    def __init__(self, user_progress: UserProgressSingleton):
+        self.user_progress = user_progress
+        self.progress = 0
+
+    def wrap_message(self, data):
+        return json.dumps({
+            "event": "progress",
+            "id": uuid.uuid4().hex,
+            "retry": self.MESSAGE_STREAM_RETRY_TIMEOUT,
+            "data": data,
+        })
+
+    async def event_loop(self, request: Request):
+        force_break_time = datetime.utcnow() + timedelta(hours=3)
+
+        while datetime.utcnow() < force_break_time:
+            if await request.is_disconnected():
+                break
+
+            progress = self.user_progress.progress
+            # progress changed
+            if self.progress != progress:
+                self.progress = progress
+
+                message = str(self.user_progress)
+                yield f'data: {self.wrap_message(message)}\n\n'
+
+                if self.user_progress.DONE:
+                    break
+
+            await asyncio.sleep(self.MESSAGE_STREAM_DELAY)
+
+    async def stream(self, request: Request):
+        return EventSourceResponse(self.event_loop(request))
