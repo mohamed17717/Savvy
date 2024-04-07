ac4f96a32b2425ca3c6581222512c1febfbe32dd
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-04-07 13:24:06 +0200

    1- create new classes for custom weighting for different websites 2- remove private prefix from method to allow inheritance to sub classes

--- App/serializers/weight_serializers.py
+++ App/serializers/weight_serializers.py
@@ -1,3 +1,4 @@
+import re
 import urllib3
 from typing import Dict
 from rest_framework import serializers
@@ -16,7 +17,7 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
     webpage_headers = serializers.SerializerMethodField()
     webpage_meta_data = serializers.SerializerMethodField()
 
-    def __weight(self, text, weight_factor) -> Dict[str, int]:
+    def get_weight(self, text, weight_factor) -> Dict[str, int]:
         weights = {}
         for word in text.split(' '):
             weights.setdefault(word, 0)
@@ -26,14 +27,14 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
         weights.pop('', None)
         return weights
 
-    def __merge_weights(self, weight1, weight2) -> Dict[str, int]:
+    def merge_weights(self, weight1, weight2) -> Dict[str, int]:
         merged = weight1.copy()
         for word, weight in weight2.items():
             merged.setdefault(word, 0)
             merged[word] += weight
         return merged
 
-    def __clean_text(self, text) -> str:
+    def clean_text(self, text) -> str:
         cleaned = (controllers.TextCleaner(text)
                    .html_entities()
                    .html_tags()
@@ -56,7 +57,7 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
                    ).text
         return cleaned
 
-    def __clean_url(self, url) -> str:
+    def clean_url(self, url) -> str:
         path = urllib3.util.parse_url(url).path
         cleaned = (controllers.TextCleaner(path)
                    .not_letters()
@@ -72,8 +73,8 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
 
     def get_url(self, obj) -> Dict[str, int]:
         WEIGHT_FACTOR = 6
-        cleaned = self.__clean_url(obj.url)
-        return self.__weight(cleaned, WEIGHT_FACTOR)
+        cleaned = self.clean_url(obj.url)
+        return self.get_weight(cleaned, WEIGHT_FACTOR)
 
     def get_title(self, obj) -> Dict[str, int]:
         WEIGHT_FACTOR = 10
@@ -86,18 +87,18 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
         if title is None:
             return {}
 
-        cleaned = self.__clean_text(title)
-        return self.__weight(cleaned, WEIGHT_FACTOR)
+        cleaned = self.clean_text(title)
+        return self.get_weight(cleaned, WEIGHT_FACTOR)
 
     def get_domain(self, obj) -> Dict[str, int]:
         WEIGHT_FACTOR = 3
         domain = obj.domain
-        return self.__weight(domain, WEIGHT_FACTOR)
+        return self.get_weight(domain, WEIGHT_FACTOR)
 
     def get_site_name(self, obj) -> Dict[str, int]:
         WEIGHT_FACTOR = 3
         site_name = obj.site_name
-        return self.__weight(site_name, WEIGHT_FACTOR)
+        return self.get_weight(site_name, WEIGHT_FACTOR)
 
     def get_webpage_headers(self, obj) -> Dict[str, int]:
         WEIGHT_FACTOR = 1
@@ -109,11 +110,11 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
         headers = wp.headers.all()
         weights = {}
         for header in headers:
-            cleaned = self.__clean_text(header.text)
+            cleaned = self.clean_text(header.text)
             weight_factor = header.weight_factor * WEIGHT_FACTOR
-            header_weight = self.__weight(cleaned, weight_factor)
+            header_weight = self.get_weight(cleaned, weight_factor)
 
-            weights = self.__merge_weights(weights, header_weight)
+            weights = self.merge_weights(weights, header_weight)
 
         return weights
 
@@ -130,11 +131,11 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
             if meta.content is None or meta.name.endswith('id'):
                 continue
 
-            cleaned = self.__clean_text(meta.content)
+            cleaned = self.clean_text(meta.content)
             weight_factor = meta.weight_factor * WEIGHT_FACTOR
-            meta_weight = self.__weight(cleaned, weight_factor)
+            meta_weight = self.get_weight(cleaned, weight_factor)
 
-            weights = self.__merge_weights(weights, meta_weight)
+            weights = self.merge_weights(weights, meta_weight)
 
         return weights
 
@@ -144,7 +145,7 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
 
         total = {}
         for weight in self.data.values():
-            total = self.__merge_weights(total, weight)
+            total = self.merge_weights(total, weight)
         return total
 
     class Meta:
@@ -158,3 +159,153 @@ class BookmarkWeightingSerializer(serializers.ModelSerializer):
             'webpage_meta_data',
         ]
 
+
+class YoutubeBookmarkWeightingSerializer(BookmarkWeightingSerializer):
+    def identify_youtube_link(self, url):
+        patterns = (
+            ('video',
+             r'youtu\.be\/[\w\-\+]+|youtube\.com\/watch\?v=[\w\-\+]+'),
+            ('channel', r'youtube\.com\/channel\/[\w\-\+]+'),
+            ('channel', r'youtube\.com\/c\/[\w\-\+]+'),
+            ('channel', r'youtube\.com\/[\@\w\-\+]+/[\w\-\+]+'),
+            ('user_channel', r'youtube\.com\/user\/[\w\-\+]+'),
+            ('playlist', r'youtube\.com\/playlist\?list=[\w\-\+]+'),
+            ('shorts', r'youtube\.com\/shorts\/[\w\-\+]+'),
+            ('search', r'youtube\.com\/results\?search_query=[\w\-\+]+')
+        )
+
+        for content_type, pattern in patterns:
+            if re.search(pattern, url):
+                return content_type
+
+        return None
+
+    def get_url(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 20
+        cleaned = self.clean_url(obj.url)
+        weight = self.get_weight(cleaned, WEIGHT_FACTOR)
+
+        content_type = self.identify_youtube_link(obj.url)
+        if content_type is not None:
+            weight['yt_' + content_type] = WEIGHT_FACTOR * 2
+
+        return weight
+
+    def get_title(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 15
+
+        title = obj.title
+        if not title:
+            return {}
+
+        cleaned = self.clean_text(title)
+        return self.get_weight(cleaned, WEIGHT_FACTOR)
+
+
+class FacebookBookmarkWeightingSerializer(BookmarkWeightingSerializer):
+    def identify_facebook_link(self, url):
+        patterns = (
+            ('page', r'facebook\.com/pg/[\w\.]+/about/?'),
+
+            ('group_member', r'facebook\.com/groups/[\w\.]+/members/?'),
+            ('group_post', r'facebook\.com/groups/[\w\.]+/posts/[\w\.]+/?'),
+            ('group_user', r'facebook\.com/groups/[\w\.]+/user/[\w\.]+/?'),
+            ('group', r'facebook\.com/groups/[\w\.]+/?'),
+
+            ('post', r'facebook\.com/[\w\.]+/posts/[\w\.]+/?'),
+            ('post', r'facebook\.com/story\.php/?\?id=[\w\.]+/?'),
+            ('post', r'facebook\.com/story\.php/?\?story_fbid=[\w\.]+/?'),
+
+            ('video', r'facebook\.com/[\w\.]+/videos/[\w\.]+/?'),
+            ('reels', r'facebook\.com/reel/[\w\.]+/?'),
+            ('story', r'facebook\.com/[\w\.]+/stories/[\w\.]+/?'),
+            ('watch', r'facebook\.com/watch/[\w\.]+/?'),
+            ('live_video', r'facebook\.com/[\w\.]+/live/?[\w\.]+?/?'),
+
+            ('event', r'facebook\.com/events/[\w\.]+/?'),
+            ('marketplace_listing',
+             r'facebook\.com/marketplace/item/[\w\.]+/?'),
+
+            ('photo', r'facebook\.com/photo\.php/?\?photo_id=[\w\.]+'),
+            ('album', r'facebook\.com/[\w\.]+/photos/[\w\.]+/?'),
+
+            ('fundraiser', r'facebook\.com/fundraisers/[\w\.]+/?'),
+
+            ('search', r'facebook\.com/search_results/?\?q=.+?/?'),
+            ('search', r'facebook\.com/search/\w+/?\?q=.+?/?'),
+
+            ('questions',
+             r'facebook\.com/questions\.php/?\?question_id=[\w\.]+/?'),
+
+            ('profile', r'facebook\.com/profile\.php/?\?id=[\w\.]+/?'),
+            ('profile', r'facebook\.com/people/.+/?'),
+            ('profile', r'facebook\.com/[\w\.]+/?'),
+        )
+
+        for content_type, pattern in patterns:
+            if re.search(pattern, url):
+                return content_type
+
+        return None
+
+    def get_url(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 20
+        cleaned = self.clean_url(obj.url)
+        weight = self.get_weight(cleaned, WEIGHT_FACTOR)
+
+        content_type = self.identify_facebook_link(obj.url)
+        if content_type is not None:
+            weight['fb_' + content_type] = WEIGHT_FACTOR * 2
+
+        return weight
+
+    def get_title(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 15
+
+        title = obj.title
+        if not title:
+            return {}
+
+        cleaned = self.clean_text(title)
+        return self.get_weight(cleaned, WEIGHT_FACTOR)
+
+
+class InstagramBookmarkWeightingSerializer(BookmarkWeightingSerializer):
+    def identify_instagram_link(self, url):
+        patterns = (
+            ('post', r'instagram\.com/p/([\.\w\-]+)/?'),
+            ('reels', r'instagram\.com/reel/([\.\w\-]+)/?'),
+            ('story', r'instagram\.com/stories/([\w\.]+)/([\d]+)/?'),
+            ('igtv', r'instagram\.com/tv/([\.\w\-]+)/?$',),
+            ('hashtag', r'instagram\.com/explore/tags/([\.\w\-]+)/?'),
+            ('search', r'instagram\.com/explore/search/keyword/?'),
+            ('profile', r'instagram\.com/([\w\.]+)/?'),
+        )
+
+        for content_type, pattern in patterns:
+            if re.search(pattern, url):
+                return content_type
+
+        return None
+
+    def get_url(self, obj) -> Dict[str, int]:
+        # Url is not important for instagram just care about the pattern
+        WEIGHT_FACTOR = 6
+        cleaned = self.clean_url(obj.url)
+        weight = self.get_weight(cleaned, WEIGHT_FACTOR)
+
+        content_type = self.identify_instagram_link(obj.url)
+        if content_type is not None:
+            weight['ig_' + content_type] = 20 * 2
+
+        return weight
+
+    def get_title(self, obj) -> Dict[str, int]:
+        WEIGHT_FACTOR = 15
+
+        title = obj.title
+        if not title:
+            return {}
+
+        cleaned = self.clean_text(title)
+        return self.get_weight(cleaned, WEIGHT_FACTOR)
