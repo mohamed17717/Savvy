44292ff6253afae089e81992ceea95c292be1b20
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-04-11 20:10:46 +0200

    1- start using bookmark hooks 2- encapsulate the code on itemloader instead of basic parse method

--- crawler/spiders/bookmark.py
+++ crawler/spiders/bookmark.py
@@ -1,7 +1,5 @@
 import scrapy
 
-from crawler.items import BookmarkItemLoader
-
 
 class BookmarkSpider(scrapy.Spider):
     name = "bookmark"
@@ -12,37 +10,19 @@ class BookmarkSpider(scrapy.Spider):
     def start_requests(self):
         # for url in self.urls:
         for bookmark in self.bookmarks:
-            payload = {'bookmark': bookmark}
-            yield scrapy.Request(
-                bookmark.url, callback=self.parse, cb_kwargs=payload, meta=payload
-            )
+            kwargs = {
+                'callback': self.parse,
+                'cb_kwargs': {'bookmark': bookmark},
+                'meta': {'bookmark': bookmark}
+            }
+            cookies = bookmark.hooks.crawler_cookies()
+            if cookies:
+                kwargs.update({'cookies': cookies})
 
-    def __get_headers(self, response):
-        def extract_headers(hx):
-            return {hx: response.xpath(f'//{hx}//text()').extract()}
-
-        headers = map(lambda i: f'h{i}', range(1, 6+1))
-        headers = map(extract_headers, headers)
-        headers_dict = {}
-        for h in headers:
-            headers_dict.update(h)
-        return headers_dict
+            yield scrapy.Request(bookmark.url, **kwargs)
 
     def parse(self, response, bookmark):
-        bookmark_item_loader = BookmarkItemLoader(response=response)
+        ItemLoader = bookmark.hooks.crawler_item_loader()
+        item_loader = ItemLoader(response=response, bookmark=bookmark)
 
-        # remove all style tags because if there is a style tag inside body, will decrease accuracy
-        response.xpath('//style').drop()
-
-        meta_tags = [meta.attrib for meta in response.xpath('//head/meta')]
-        page_title = response.xpath('//head/title/text()').extract_first()
-        url = response.url
-        headers = self.__get_headers(response)
-
-        bookmark_item_loader.add_value('meta_tags', meta_tags)
-        bookmark_item_loader.add_value('page_title', page_title)
-        bookmark_item_loader.add_value('url', url)
-        bookmark_item_loader.add_value('headers', headers)
-        bookmark_item_loader.add_value('bookmark', bookmark)
-
-        yield bookmark_item_loader.load_item()
+        yield item_loader.load_item()
