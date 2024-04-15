2d75b8796fb2395f984895e40a48cf9c9c163a75
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-04-15 12:09:33 +0200

    rename this file to import it easier from shell

--- resources/cshell.py
+++ resources/cshell.py
@@ -1,29 +1,37 @@
 
 from App import models
 from django.db.models import Count
-from pprint import pprint
+from django.db import models as dj_models
+
 
 def cluster_duplicates_bookmarks():
     return models.Bookmark.objects.annotate(
         clusters_count=Count('clusters')).filter(clusters_count__gt=1)
 
+
 def not_clustered_bookmarks():
     return models.Bookmark.objects.filter(clusters__isnull=True)
+
 
 def one_bookmark_cluster():
     return models.Cluster.objects.filter(bookmarks_count=1)
 
+
 def important_words_percent():
     return models.WordWeight.objects.filter(important=True).count() / models.WordWeight.objects.count() * 100
+
 
 def cluster_related_to_tag(tag_name):
     return models.Cluster.objects.filter(bookmarks__tags__name=tag_name).distinct()
 
+
 def cluster_by_name(name):
     return models.Cluster.objects.get(name=name)
 
+
 def words_vectors_for_cluster(cluster_name):
     return [bm.important_words for bm in models.Cluster.objects.get(name=cluster_name).bookmarks.all()]
+
 
 def how_it_look(cluster_name, index):
     cluster = cluster_by_name(cluster_name)
@@ -32,7 +40,18 @@ def how_it_look(cluster_name, index):
     return 'http://localhost' + html_file.url, bookmark
 
 
-bms = not_clustered_bookmarks()
+def delete_everything():
+    for model_name in dir(models):
+        model = getattr(models, model_name)
+        try:
+            if issubclass(model, dj_models.Model) and 'App' in model.__module__:
+                print('Start deleting', model)
+                model.objects.all().delete()
+        except Exception:
+            pass
+
+
+# bms = not_clustered_bookmarks()
 
 # sorted(cluster_duplicates_bookmarks().values_list('clusters_count', flat=True))
 # cluster_duplicates_bookmarks().count() # 168
