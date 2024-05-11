54208e788f2068e6c83da768052aad5a7085d6db
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-05-11 15:40:29 +0300

    make full text search allow exclude

--- common/utils/drf/filters.py
+++ common/utils/drf/filters.py
@@ -1,6 +1,7 @@
 from django.contrib.postgres.search import SearchQuery, SearchVector
 
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, search_smart_split
+from rest_framework.fields import CharField
 
 
 class FullTextSearchFilter(SearchFilter):
@@ -17,14 +18,28 @@ class FullTextSearchFilter(SearchFilter):
         if query:
             return SearchQuery(' '.join(query))
 
+    def get_exclude_terms(self, request) -> str:
+        value = request.query_params.get('exclude', '')
+        field = CharField(trim_whitespace=False, allow_blank=True)
+        cleaned_value = field.run_validation(value)
+        query = search_smart_split(cleaned_value)
+
+        if query:
+            return SearchQuery(' '.join(query))
+
     def filter_queryset(self, request, queryset, view):
         search_fields = self.get_search_fields(view, request)
         search_terms = self.get_search_terms(request)
+        exclude_terms = self.get_exclude_terms(request)
 
-        if not search_fields or not search_terms:
+        if not search_fields or (not search_terms and not exclude_terms):
             return queryset
 
         vector = SearchVector(*search_fields)
-        queryset = queryset.annotate(search=vector).filter(search=search_terms)
+        queryset = queryset.annotate(search=vector)
+        if search_terms:
+            queryset = queryset.filter(search=search_terms)
+        elif exclude_terms:
+            queryset = queryset.exclude(search=exclude_terms)
 
         return queryset.distinct('id')
