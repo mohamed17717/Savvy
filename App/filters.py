021659a256b536867021c66fc94642b0efb32d71
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-05-11 15:08:30 +0300

    create all needed filters for bookmarks to navigate them very efficicent

--- App/filters.py
+++ App/filters.py
@@ -1,4 +1,7 @@
+from django.db.models import Q
+
 from django_filters import rest_framework as filters
+from django_filters.filters import BaseInFilter
 
 from App import models
 
@@ -21,6 +24,10 @@ class TagFilter(filters.FilterSet):
         fields = ['name']
 
 
+class ListFilter(BaseInFilter, filters.CharFilter):
+    pass
+
+
 class BookmarkFilter(filters.FilterSet):
     status = filters.NumberFilter('user_status')
     process_status = filters.NumberFilter('process_status')
@@ -33,6 +40,23 @@ class BookmarkFilter(filters.FilterSet):
     node = filters.NumberFilter('nodes__path', lookup_expr='icontains')
 
     exclude = filters.CharFilter(method='filter_exclude')
+
+    websites = ListFilter(field_name='website_id', lookup_expr='in')
+    exclude_websites = ListFilter(
+        field_name='website_id', lookup_expr='in', exclude=True)
+
+    topics = ListFilter(field_name='tags__id', lookup_expr='in')
+    exclude_topics = ListFilter(
+        field_name='tags__id', lookup_expr='in', exclude=True)
+
+    seen = filters.BooleanFilter('history', lookup_expr='isnull', exclude=True)
+    dead = filters.BooleanFilter('scrapes__status_code', method='filter_dead')
+
+    def filter_dead(self, queryset, name, value):
+        query = Q(scrapes__isnull=True) | Q(scrapes__status_code=200)
+        if value:
+            return queryset.exclude(query)
+        return queryset.filter(query)
 
     def filter_exclude(self, queryset, name, value):
         return queryset.exclude(
