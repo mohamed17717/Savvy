3a5f4225986cd8cd63d714cd4b12892b3f761b06
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-05-12 14:07:28 +0300

    more functions to analyze pricing and taxes stuff

--- resources/AnalMetrics.py
+++ resources/AnalMetrics.py
@@ -23,4 +23,21 @@ real_pay = {
     'server_contabo': [11]
 }
 
+def pr(x, cost_per_month, users=1000):
+    import numpy as np
+    cost = x * 2.9 / 100 + 0.30
+    monthly_per_user = cost_per_month / users
+    revenue = x - cost - monthly_per_user
+    taxes = 0
+    if revenue > 0:
+        taxes = revenue * 20 / 100
+    return np.array([revenue - taxes , cost, monthly_per_user , taxes])
 
+
+pr(12, 2500, users:=1000) * users * 12
+pr(12, 5_000, users:=10_000) * users * 12
+r = pr(12, 20_000, users:=100_000) * users * 12
+r = pr(12, 30_000, users:=1_000_000) * users * 12
+print(r, f'{r[0] / sum(r) * 100:.2f}%', sep='\n')
+
+pr(12, 1000, users:=1000) * users * 12
