f7a41bd11ffe1e5cd47baedf9aa8e032cb59a9d7
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-05-13 16:25:00 +0300

    full text search now can handle multiple search query for include and exclude operations

--- common/utils/drf/filters.py
+++ common/utils/drf/filters.py
@@ -13,24 +13,37 @@ class FullTextSearchFilter(SearchFilter):
 
         return fields
 
-    def get_search_terms(self, request) -> str:
-        query = super().get_search_terms(request)
-        if query:
-            return SearchQuery(' '.join(query))
+    def get_raw_search_terms(self, request, search_param=None):
+        value = request.query_params.get(search_param or self.search_param, '')
 
-    def get_exclude_terms(self, request) -> str:
-        value = request.query_params.get('exclude', '')
         field = CharField(trim_whitespace=False, allow_blank=True)
         cleaned_value = field.run_validation(value)
+        cleaned_value = cleaned_value.replace(',', '|')
         query = search_smart_split(cleaned_value)
 
+        def to_raw(q):
+            def quote(i): return f"'{i.strip()}'"
+            def parentheses(i): return f"({i.strip()})"
+            def or_join(i): return ' | '.join(i)
+            def and_join(i): return ' & '.join(i)
+            def is_multiple(i): return ' ' in i
+            def multiple_query(i): return parentheses(and_join(map(quote, i.split(' '))))
+
+            q = ' '.join(query)
+            q = q.split('|')
+            q = or_join([
+                multiple_query(i) if is_multiple(i) else quote(i) for i in q
+            ])
+            return q
+
         if query:
-            return SearchQuery(' '.join(query))
+            query = to_raw(query)
+            return SearchQuery(query, search_type='raw')
 
     def filter_queryset(self, request, queryset, view, distinct=True):
         search_fields = self.get_search_fields(view, request)
-        search_terms = self.get_search_terms(request)
-        exclude_terms = self.get_exclude_terms(request)
+        search_terms = self.get_raw_search_terms(request, search_param='search')
+        exclude_terms = self.get_raw_search_terms(request, search_param='exclude')
 
         if not search_fields or (not search_terms and not exclude_terms):
             return queryset
