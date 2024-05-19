602ba109dae63102216bba505ce23accc7b7f464
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-05-19 14:14:00 +0300

    compress nodes with less the 3 bookmarks

--- App/controllers/graph_builder.py
+++ App/controllers/graph_builder.py
@@ -54,13 +54,14 @@ class WordGraphBuilder:
     def group_to_sub_matrix(self, group: list[int]) -> list[list[float]]:
         return self.similarity_matrix[np.ix_(group, group)]
 
-    def store_group(self, group: list[int], is_leaf=True):
+    def store_group(self, group: list[int], is_leaf=True, is_sharded_islands=False):
         node_kwargs = {
             'user': self.user,
             'parent': self.parent,
             'threshold': (self.threshold / 100),
             'bookmarks_count': len(group),
-            'is_leaf': is_leaf
+            'is_leaf': is_leaf,
+            'is_sharded_islands': is_sharded_islands
         }
 
         if is_leaf:
@@ -92,17 +93,33 @@ class WordGraphBuilder:
 
     def build(self):
         groups = self.find_groups()
+        lengths = list(map(len, groups))
+        is_sharded = [False] * len(groups)
+
+        sharded_islands = []
+        for index in range(len(groups)):
+            if lengths[index] <= 3:
+                sharded_islands.extend(groups[index])
+                is_sharded[index] = True
+
+        groups = [group for group, is_sharded in zip(groups, is_sharded) if not is_sharded]
+        lengths = [length for length, is_sharded in zip(lengths, is_sharded) if not is_sharded]
+        # groups.append(sharded_islands)
+
         first_level = self.parent is None
         last_level = self.threshold >= self.MAXIMUM_THRESHOLD
 
         # if groups are too small don't nest new level and increase threshold
         is_leaf_array = [
-            last_level or len(group) <= self.ACCEPTED_LEAF_LENGTH
-            for group in groups
+            last_level or length <= self.ACCEPTED_LEAF_LENGTH
+            for length in lengths 
         ]
         nodes_count = is_leaf_array.count(False)
-        is_there_enough_nodes = first_level or last_level or nodes_count >= 2
+        if sharded_islands:
+            self.store_group(sharded_islands, True, True)
+            nodes_count += 1
 
+        is_there_enough_nodes = first_level or last_level or nodes_count >= 2
         for is_leaf, group in zip(is_leaf_array, groups):
             if is_leaf or is_there_enough_nodes:
                 node = self.store_group(group, is_leaf)
