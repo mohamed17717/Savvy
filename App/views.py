9b8ce25d2c36dc1e73cbc245c1f4d1bc252bf03b
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-05-28 21:49:56 +0300

    Bookmarks: Endpoints for [permanentDelete, archived delete, restore, open from short url, archived list, deleted list, favorite list]

--- App/views.py
+++ App/views.py
@@ -9,9 +9,12 @@ from django.utils import timezone
 
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.generics import ListAPIView
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
 
 from App import serializers, filters, models
 
@@ -66,11 +69,13 @@ class BookmarkAPI(RUDLViewSet):
     def get_serializer_class(self):
         serializer_class = self.serializer_class
 
+        list_actions = [
+            'list', 'archived_list', 'deleted_list', 'favorite_list', 'retrieve'
+        ]
+
         if self.action == 'update' or self.action == 'partial_update':
             serializer_class = serializers.BookmarkSerializer.BookmarkUpdate
-        elif self.action == 'list':
-            serializer_class = serializers.BookmarkSerializer.BookmarkDetails
-        elif self.action == 'retrieve':
+        elif self.action in list_actions:
             serializer_class = serializers.BookmarkSerializer.BookmarkDetails
 
         return serializer_class
@@ -81,12 +86,13 @@ class BookmarkAPI(RUDLViewSet):
 
         qs = self.request.user.bookmarks.all()
 
-        if self.action == 'update' or self.action == 'partial_update':
-            pass
-        elif self.action == 'list':
-            pass
-        elif self.action == 'retrieve':
-            pass
+        archive_actions = [
+            'permanent_delete', 'restore', 'archived_destroy', 'archived_list', 'deleted_list']
+        all_actions = ['open_url']
+        if self.action in archive_actions:
+            qs = models.Bookmark.hidden_objects.all().by_user(self.request.user)
+        elif self.action in all_actions:
+            qs = models.Bookmark.all_objects.all().by_user(self.request.user)
 
         return qs
 
@@ -94,6 +100,56 @@ class BookmarkAPI(RUDLViewSet):
         instance.hidden = True
         instance.delete_scheduled_at = timezone.now() + timedelta(days=14)
         instance.save(update_fields=['hidden', 'delete_scheduled_at'])
+
+    @action(methods=['delete'], detail=False, url_path=r'(?P<pk>[^/.]+)/permanent-delete')
+    def permanent_delete(self, request, pk):
+        instance = get_object_or_404(self.get_queryset(), pk=pk)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['get'], detail=False, url_path=r'(?P<pk>[^/.]+)/restore')
+    def restore(self, request, pk):
+        instance = get_object_or_404(self.get_queryset(), pk=pk)
+        instance.hidden = False
+        instance.delete_scheduled_at = None
+        instance.save(update_fields=['hidden', 'delete_scheduled_at'])
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['delete'], detail=False, url_path=r'(?P<pk>[^/.]+)/archived-delete$')
+    def archived_destroy(self, request, pk):
+        instance = get_object_or_404(self.get_queryset(), pk=pk)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['get'], detail=False, url_path=r'(?P<uuid>[^/.]+)/open', permission_classes=[AllowAny])
+    def open_url(self, request, uuid):
+        bookmark = get_object_or_404(models.Bookmark.objects.all(), uuid=uuid)
+        models.BookmarkHistory.objects.create(bookmark=bookmark)
+        return HttpResponseRedirect(bookmark.url)
+
+    @action(methods=['get'], detail=False, url_path='archived-list')
+    def archived_list(self, request):
+        qs = self.get_queryset()
+        qs = qs.filter(delete_scheduled_at__isnull=True)
+        serializer_class = self.get_serializer_class()
+        data = serializer_class(qs, many=True).data
+        return Response(data)
+
+    @action(methods=['get'], detail=False, url_path='deleted-list')
+    def deleted_list(self, request):
+        qs = self.get_queryset()
+        qs = qs.filter(delete_scheduled_at__isnull=False)
+        serializer_class = self.get_serializer_class()
+        data = serializer_class(qs, many=True).data
+        return Response(data)
+
+    @action(methods=['get'], detail=False, url_path='favorite-list')
+    def favorite_list(self, request):
+        qs = self.get_queryset()
+        qs = qs.filter(favorite=True)
+        serializer_class = self.get_serializer_class()
+        data = serializer_class(qs, many=True).data
+        return Response(data)
 
 
 class TagAPI(RULViewSet):
@@ -142,15 +198,6 @@ class TagListAPI(ListAPIView):
         if self.request.user.is_anonymous:
             return models.Tag.objects.none()
         return self.request.user.tags.all()
-
-
-class BookmarkShortAPI(APIView):
-    permission_classes = [AllowAny]
-
-    def get(self, request, uuid):
-        bookmark = get_object_or_404(models.Bookmark.objects.all(), uuid=uuid)
-        models.BookmarkHistory.objects.create(bookmark=bookmark)
-        return HttpResponseRedirect(bookmark.url)
 
 
 class WordGraphNodeAPI(APIView):
