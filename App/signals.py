2fa10d8ff5c36d87c38e8e13fbd25ecb219ac50a
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-02 03:52:14 +0300

    Bookmark FIle Signal: move work to the task to make the response latency very small

--- App/signals.py
+++ App/signals.py
@@ -4,27 +4,16 @@ from django.dispatch import receiver
 
 from App import models, tasks
 
-from realtime.common.redis_utils import RedisPubSub
-
 
 @receiver(post_save, sender=models.BookmarkFile)
 def on_create_bookmark_file_extract_urls(sender, instance, created, **kwargs):
     if not created:
         return
 
-    def event():
-        links = instance.cleaned_bookmarks_links()
-        tasks.store_bookmarks_task.delay(instance.id, links)
-
-        RedisPubSub.pub({
-            'type': RedisPubSub.MessageTypes.FILE_UPLOAD,
-            'user_id': instance.user.id,
-            'total_bookmarks': len(links),
-        })
-
-    transaction.on_commit(event)
+    transaction.on_commit(
+        lambda: tasks.store_bookmarks_task.delay(instance.id))
 
 
 @receiver(pre_save, sender=models.BookmarkFile)
 def on_save_bookmark_file_validate_file_content(sender, instance, **kwargs):
-    instance.file_obj.validate(raise_exception=True)
+    instance.file_obj.validate()
