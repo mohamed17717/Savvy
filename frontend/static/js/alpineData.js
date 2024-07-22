1f732d522477c104cc9bc9d632a5ddcf4ca6e278
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-07-22 09:11:46 +0300

    Frontend: - Serve frontend from nginx so i copied the build to the backend folder

---
+++ frontend/static/js/alpineData.js
@@ -0,0 +1,337 @@
+class xProgressToastModal {
+  constructor() {
+    this.progress = {
+      percent: 50,
+      message: "loading is in progress",
+    };
+  }
+
+  init() {
+    let eventSource = new EventSource("http://localhost/fast/progress", {
+      // let eventSource = new EventSource("http://62.171.141.141/fast/progress", {
+      withCredentials: true,
+    });
+
+    eventSource.onopen = () => {
+      document.querySelector("#progressToastModal")?.show();
+
+      eventSource.onmessage = (event) => {
+        const message = JSON.parse(event.data);
+        const data = JSON.parse(message.data);
+        this.progress.percent = data.progress;
+        this.progress.message = data.message;
+        if (this.progress.percent === 100) {
+          eventSource.close();
+          document.querySelector("#progressToastModal")?.remove();
+          // TODO inject cookie or local storage that its completed don't show it now
+          // and when upload change this value to wait
+
+          // setTimeout(() => {
+          //   window.location.reload();
+          // }, 2000);
+        }
+      };
+    };
+  }
+}
+
+class xUploadBookmarksModal {
+  constructor() {
+    this.loading = false;
+    this.progress = { percent: 0, message: "" };
+  }
+}
+
+class xDashboardDetails {
+  constructor() {
+    this.bookmarks = [];
+    this.totalBookmarks = 0;
+    this.nextPage = null;
+    this.view = "grid"; // TODO save latest view user choosed
+    this.loadingNewBookmarks = false;
+    this.loadingBookmarks = false;
+    this.openFilterButtons = false;
+    this.defaultIcon =
+      "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1655817725/logos/community";
+
+    this.searchQuery = new URLSearchParams(window.location.search);
+    if (this.searchQuery.has("metaData")) {
+      this.metaData = JSON.parse(this.searchQuery.get("metaData"));
+    } else {
+      this.metaData = {};
+    }
+
+    this.sideBarActive = this.searchQuery.get("sidebar") || "all"; // 'all'; // TODO save latest view user choosed
+
+    this.appliedFilters = this.appliedFiltersToHumanArray();
+
+    this.graphStarted = false;
+    this.initialized = false;
+  }
+
+  appliedFiltersToHumanArray() {
+    const filters = [];
+    this.searchQuery.entries().forEach(([key, value]) => {
+      if (key === "metaData") return;
+      if (key === "sidebar") return;
+      value.split(",").forEach((val) => {
+        filters.push(`${key}: ${val}`);
+      });
+    });
+    return filters;
+  }
+
+  translateFilterNumber(filter) {
+    let [key, value] = filter.split(": ");
+    value = value.trim();
+
+    function isNumber(num) {
+      if (num === "" || num === null) {
+        return false;
+      }
+      return !isNaN(num);
+    }
+
+    if (isNumber(value)) {
+      const newValue = this.metaData[value] || value;
+      return `${key}: ${newValue}`;
+    }
+
+    return filter;
+  }
+
+  insertToAppliedFilters(key, value, reload = true) {
+    if (this.searchQuery.has(key)) {
+      // if has same value skip
+      const oldValue = this.searchQuery.get(key);
+      if (oldValue.split(",").includes(value)) return;
+
+      this.searchQuery.set(key, `${value},${this.searchQuery.get(key)}`);
+    } else {
+      this.searchQuery.append(key, value);
+    }
+    if (reload) {
+      window.history.replaceState({}, "", `?${this.searchQuery.toString()}`);
+      this.appliedFilters = this.appliedFiltersToHumanArray();
+      this.reloadBookmarks();
+    }
+  }
+
+  updateAppliedFilter(key, value) {
+    this.searchQuery.set(key, value);
+    window.history.replaceState({}, "", `?${this.searchQuery.toString()}`);
+    this.appliedFilters = this.appliedFiltersToHumanArray();
+    this.reloadBookmarks();
+  }
+
+  removeAppliedFilter(key) {
+    this.searchQuery.delete(key);
+    window.history.replaceState({}, "", `?${this.searchQuery.toString()}`);
+    this.appliedFilters = this.appliedFiltersToHumanArray();
+    this.reloadBookmarks();
+  }
+
+  removeFromAppliedFilters(key, value, reload = true) {
+    if (this.searchQuery.has(key)) {
+      let oldValue = this.searchQuery.get(key).split(",");
+      oldValue.splice(oldValue.indexOf(value), 1);
+      oldValue = oldValue.join(",");
+      if (oldValue === "") this.searchQuery.delete(key);
+      else this.searchQuery.set(key, oldValue);
+    }
+    if (reload) {
+      window.history.replaceState({}, "", `?${this.searchQuery.toString()}`);
+      this.appliedFilters = this.appliedFiltersToHumanArray();
+      this.reloadBookmarks();
+    }
+  }
+
+  reloadBookmarks() {
+    this.bookmarks = [];
+    this.nextPage = null;
+    this.totalBookmarks = 0;
+    this.loadingNewBookmarks = false;
+    this.loadingBookmarks = false
+    this.initialized = false;
+    this.init();
+    this.openFilterButtons && dispatchEvent(new Event("open"));
+  }
+
+  async init() {
+    if (this.initialized) return;
+    this.initialized = true;
+    this.loadingBookmarks = true
+    let response = await bookmarkList(this.searchQuery, this.sideBarActive);
+    this.totalBookmarks = response.count;
+    this.bookmarks = response.results;
+    this.loadingBookmarks = false
+    if (response.next) this.nextPage = urlToPath(response.next);
+    else this.nextPage = null;
+
+    setTimeout(() => {
+      document.querySelector("#loadingPage")?.remove();
+    }, 0);
+
+    document.addEventListener("scroll", async () => {
+      const yAxis = window.scrollY + window.innerHeight;
+      if (yAxis >= document.body.scrollHeight - 700) {
+        if (this.nextPage && !this.loadingNewBookmarks) {
+          this.loadingNewBookmarks = true;
+          response = await get(this.nextPage);
+          this.bookmarks = [...this.bookmarks, ...response.results];
+          if (response.next) this.nextPage = urlToPath(response.next);
+          else this.nextPage = null;
+          this.loadingNewBookmarks = false;
+        }
+      }
+    });
+  }
+}
+
+class xBookmarkFiltersModal {
+  constructor() {
+    // TODO search
+    this.websites = [];
+    this.websitesNextURL = null;
+    this.websiteSearchParam = "website_search";
+    this.websiteLoading = false;
+    this.websiteSearchTask = null;
+
+    this.topics = [];
+    this.topicsNextURL = null;
+    this.topicsSearchParam = "tags_search";
+    this.topicLoading = false;
+    this.topicSearchTask = null;
+  }
+
+  onOpen(data) {
+    bookmarkFilterWebsiteChoices(data.searchQuery).then((data) => {
+      this.websites = data.results;
+      this.websitesNextURL = urlToPath(data.next);
+    });
+    bookmarkFilterTopicChoices(data.searchQuery).then((data) => {
+      this.topics = data.results;
+      this.topicsNextURL = urlToPath(data.next);
+    });
+  }
+  onClose() {
+    this.websites = [];
+    this.websitesNextURL = null;
+    this.topics = [];
+    this.topicsNextURL = null;
+  }
+
+  loadMoreWebsites() {
+    if (!this.websitesNextURL || this.websiteLoading) return;
+    this.websiteLoading = true;
+    get(this.websitesNextURL).then((data) => {
+      this.websites = [...this.websites, ...data.results];
+      this.websitesNextURL = urlToPath(data.next);
+      this.websiteLoading = false;
+    });
+  }
+
+  loadMoreTopics() {
+    if (!this.topicsNextURL || this.topicLoading) return;
+    this.topicLoading = true;
+    get(this.topicsNextURL).then((data) => {
+      this.topics = [...this.topics, ...data.results];
+      this.topicsNextURL = urlToPath(data.next);
+      this.topicLoading = false;
+    });
+  }
+
+  searchWebsites(data, query) {
+    if (this.websiteSearchTask) clearTimeout(this.websiteSearchTask);
+
+    this.websiteSearchTask = setTimeout(() => {
+      let searchQuery = new URLSearchParams(data.searchQuery.toString());
+      searchQuery.set(this.websiteSearchParam, query);
+      bookmarkFilterWebsiteChoices(searchQuery).then((data) => {
+        this.websites = data.results;
+        this.websitesNextURL = urlToPath(data.next);
+      });
+    }, 500);
+  }
+
+  searchTopics(data, query) {
+    if (this.topicSearchTask) clearTimeout(this.topicSearchTask);
+
+    this.topicSearchTask = setTimeout(() => {
+      let searchQuery = new URLSearchParams(data.searchQuery.toString());
+      searchQuery.set(this.topicsSearchParam, query);
+      bookmarkFilterTopicChoices(searchQuery).then((data) => {
+        this.topics = data.results;
+        this.topicsNextURL = urlToPath(data.next);
+      });
+    }, 500);
+  }
+
+  unpackWebsiteForChange(website) {
+    return {
+      key: "websites",
+      excludeKey: "exclude_websites",
+      id: website.id,
+      humanText: website.domain,
+    };
+  }
+
+  unpackTopicForChange(topic) {
+    return {
+      key: "topics",
+      excludeKey: "exclude_topics",
+      id: topic.id,
+      humanText: topic.name,
+    };
+  }
+
+  onChangeChoice($el, data, { key, excludeKey, id, humanText }) {
+    if ($el.checked) {
+      data.metaData[id] = humanText;
+      data.searchQuery.set("metaData", JSON.stringify(data.metaData));
+
+      data.insertToAppliedFilters(key, id);
+    } else {
+      delete data.metaData[key];
+      data.searchQuery.set("metaData", JSON.stringify(data.metaData));
+
+      data.removeFromAppliedFilters(key, id);
+      data.removeFromAppliedFilters(excludeKey, id);
+    }
+  }
+
+  choiceIsChecked(data, { key, excludeKey, id }) {
+    return (
+      (data.searchQuery.has(key) &&
+        data.searchQuery.get(key).split(",").includes(id.toString())) ||
+      (data.searchQuery.has(excludeKey) &&
+        data.searchQuery.get(excludeKey).split(",").includes(id.toString()))
+    );
+  }
+
+  choiceIsExcluded(data, { excludeKey, id }) {
+    return (
+      data.searchQuery.has(excludeKey) &&
+      data.searchQuery.get(excludeKey).split(",").includes(id.toString())
+    );
+  }
+}
+
+class xCookieAlert {
+  constructor() {
+    this.cookiePromptShow = false;
+  }
+
+  cookiePrompt(status) {
+    localStorage.setItem("cookieStatus", status);
+    document.querySelector("#cookieAlert")?.remove();
+  }
+
+  init() {
+    setTimeout(() => {
+      if (localStorage.getItem("cookieStatus") !== "true") {
+        this.cookiePromptShow = true;
+      }
+    }, 10000);
+  }
+}
---
+++ frontend/static/js/auth.js
@@ -0,0 +1,356 @@
+class goto {
+  static dashboard() {
+    window.location.href = "/f/dashboard";
+  }
+
+  static login() {
+    window.location.href = "/f/auth";
+  }
+}
+
+class auth {
+  static getToken() {
+    let token = localStorage.getItem("token");
+    let expiry = localStorage.getItem("expiry");
+
+    if (token && expiry) {
+      if (new Date().getTime() > expiry) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("expiry");
+        token = null;
+        expiry = null;
+      }
+    }
+
+    return token;
+  }
+
+  static setToken({ token, expiry }) {
+    if (!token) return;
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("expiry", expiry);
+  }
+
+  static removeToken() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiry");
+  }
+}
+
+class Backend {
+  constructor(headers = {}) {
+    this.BASE_URL = "http://localhost";
+    // this.BASE_URL = "http://62.171.141.141";
+    this.headers = headers;
+
+    let token = auth.getToken();
+    if (token) {
+      this.headers["Authorization"] = `Token ${token}`;
+    }
+  }
+  request(path, method, data) {
+    if (!path.startsWith("/")) throw new Error("Path must start with /");
+
+    let url = this.BASE_URL + path;
+
+    const requestSetup = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        ...this.headers,
+      },
+      credentials: "include",
+    };
+    if (data) {
+      requestSetup.body = JSON.stringify(data);
+    }
+
+    return fetch(url, requestSetup)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+        if (response.status === 204) return null;
+        return response.json();
+      })
+      .then((data) => data)
+      .catch((error) => {
+        errorToast(error.message);
+      });
+  }
+
+  get(url) {
+    return this.request(url, "GET", null);
+  }
+
+  post(url, data) {
+    if (!url.endsWith("/")) url += "/";
+    return this.request(url, "POST", data);
+  }
+
+  patch(url, data) {
+    if (!url.endsWith("/")) url += "/";
+    return this.request(url, "PATCH", data);
+  }
+
+  delete(url, data) {
+    if (!url.endsWith("/")) url += "/";
+    return this.request(url, "DELETE", data);
+  }
+
+  getFormData(form) {
+    const formData = new FormData(form);
+    const values = [...formData.entries()];
+    const data = {};
+    for (const [key, value] of values) {
+      data[key] = value;
+    }
+    return data;
+  }
+}
+
+function errorToast(message) {
+  const toast = document.createElement("div");
+  toast.innerHTML = toastModal.innerHTML.replace(/\$\{message\}/, message);
+
+  document.body.appendChild(toast);
+  setTimeout(() => {
+    toast.remove();
+  }, 5000);
+}
+
+function submitAuthForm($event) {
+  $event.preventDefault();
+
+  const backend = new Backend();
+
+  const form = $event.target;
+  const data = backend.getFormData(form);
+  const url = form.getAttribute("action");
+
+  return backend.post(url, data).then((data) => {
+    auth.setToken(data);
+    goto.dashboard();
+  });
+}
+
+function get(path) {
+  // TODO: remove this function
+  const backend = new Backend();
+  return backend.get(path);
+}
+
+function tagsList() {
+  const path = "/bm/tag/list/";
+
+  return get(path).then((data) => {
+    const weights = data.map((item) => item.weight);
+    const stepsCount = data.length;
+    const maxWeight = Math.max(...weights);
+    const minWeight = 0; //Math.min(...weights)
+    const weightStep = (maxWeight - minWeight) / stepsCount;
+    const maxRem = 2.5;
+    const minRem = 0.5;
+    const remStep = maxRem / stepsCount;
+
+    for (let item of data) {
+      newWeight = (item.weight / weightStep) * remStep + minRem;
+      item.weight = parseFloat(newWeight.toFixed(2));
+    }
+
+    return data.sort(() => Math.random() - 0.5);
+  });
+}
+
+function bookmarkList(searchQuery, listType = "all") {
+  let path = {
+    all: "/bm/bookmark/list/",
+    favorite: "/bm/bookmark/favorite-list/",
+    archive: "/bm/bookmark/archived-list/",
+    trash: "/bm/bookmark/deleted-list/",
+    history: "/bm/bookmark/history-list/",
+    collections: "/bm/bookmark/list/",
+  }[listType];
+
+  if (searchQuery?.size > 0) {
+    path += `?${searchQuery.toString()}`;
+  }
+
+  return get(path);
+}
+
+function bookmarkFilterWebsiteChoices(searchQuery) {
+  let path = "/bm/filter/choices/website/";
+
+  if (searchQuery) {
+    let searchQueryClone = new URLSearchParams(searchQuery?.toString());
+
+    searchQueryClone?.delete("websites");
+    searchQueryClone?.delete("exclude_websites");
+
+    if (searchQueryClone?.size > 0) {
+      path += `?${searchQueryClone.toString()}`;
+    }
+  }
+
+  return get(path);
+}
+
+function graphRoots(searchQuery) {
+  let path = "/bm/graph/";
+
+  if (searchQuery) {
+    let searchQueryClone = new URLSearchParams(searchQuery?.toString());
+
+    searchQueryClone?.delete("node");
+
+    if (searchQueryClone?.size > 0) {
+      path += `?${searchQueryClone.toString()}`;
+    }
+  }
+
+  return get(path);
+}
+
+function graphChildren(parentId, searchQuery) {
+  let path = `/bm/graph/${parentId}`;
+
+  if (searchQuery) {
+    let searchQueryClone = new URLSearchParams(searchQuery?.toString());
+
+    searchQueryClone?.delete("node");
+
+    if (searchQueryClone?.size > 0) {
+      path += `?${searchQueryClone.toString()}`;
+    }
+  }
+
+  return get(path);
+}
+
+function bookmarkFilterTopicChoices(searchQuery) {
+  let path = "/bm/filter/choices/topic/";
+
+  if (searchQuery) {
+    let searchQueryClone = new URLSearchParams(searchQuery?.toString());
+
+    searchQueryClone?.delete("topics");
+    searchQueryClone?.delete("exclude_topics");
+
+    if (searchQueryClone?.size > 0) {
+      path += `?${searchQueryClone.toString()}`;
+    }
+  }
+
+  return get(path);
+}
+
+function logout() {
+  const path = "/users/logout/";
+  return get(path);
+}
+
+function getCard(bookmarkId) {
+  return document.querySelector(`[data-card-id="${bookmarkId}"]`);
+}
+
+function favoriteBookmark(bookmarkId, favorite) {
+  const path = `/bm/bookmark/${bookmarkId}/`;
+  const backend = new Backend();
+  const data = { favorite };
+
+  return backend.patch(path, data);
+}
+
+function archiveBookmark(bookmarkId) {
+  const path = `/bm/bookmark/${bookmarkId}/`;
+  const backend = new Backend();
+  const data = { hidden: true };
+
+  return backend.patch(path, data);
+}
+
+function deleteBookmark(bookmarkId) {
+  const path = `/bm/bookmark/${bookmarkId}/`;
+  const backend = new Backend();
+
+  return backend.delete(path);
+}
+
+function deleteArchivedBookmark(bookmarkId) {
+  const path = `/bm/bookmark/${bookmarkId}/archived-delete/`;
+  const backend = new Backend();
+
+  return backend.delete(path);
+}
+
+function permenantDeleteBookmark(bookmarkId) {
+  const path = `/bm/bookmark/${bookmarkId}/permanent-delete/`;
+  const backend = new Backend();
+
+  return backend.delete(path);
+}
+
+function restoreBookmark(bookmarkId) {
+  const path = `/bm/bookmark/${bookmarkId}/restore/`;
+  return get(path);
+}
+
+function rcToggleStatus(rc, done) {
+  const path = `/bm/bookmark/${rc}/`;
+
+  const backend = new Backend();
+  const data = {
+    user_status: done ? 2 : 1,
+  };
+
+  return backend.patch(path, data);
+}
+
+function rcDelete(rc) {
+  const path = `/bm/bookmark/${rc}/`;
+  const backend = new Backend();
+  return backend.delete(path);
+}
+
+function validateSize(input) {
+  const fileSize = input.files[0].size / 1024 / 1024; // in MiB
+  if (fileSize > 5) {
+    errorToast("File size exceeds 5 MiB");
+    input.value = "";
+    return false;
+  }
+}
+
+function submitBookmarkFile(form) {
+  const headers = {
+    // "Content-Type": "multipart/form-data",
+    Authorization: `Token ${auth.getToken()}`,
+  };
+  const data = new FormData(form);
+  const fetchSetup = {
+    method: "POST",
+    headers: headers,
+    body: data,
+    credentials: "include",
+  };
+
+  return (
+    fetch(`http://localhost/bm/file/create/`, fetchSetup)
+      // return fetch(`http://62.171.141.141/bm/file/create/`, fetchSetup)
+      .then(async (response) => {
+        if (!response.ok) throw new Error((await response.json()).error);
+        return response.json();
+      })
+      .then((data) => data)
+      .catch((error) => {
+        errorToast(error.message);
+        return false;
+      })
+  );
+}
+
+function urlToPath(url) {
+  if(!url) return url
+  const urlParts = new URL(url);
+  return urlParts.pathname + urlParts.search;
+}
