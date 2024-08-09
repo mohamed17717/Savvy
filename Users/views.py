07121d3b659c2385274fbbefa19839eb0ab652aa
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-08-09 09:58:27 +0300

    Logout Views: - Override knox views to make sure to delete user jwt token on logout also

--- Users/views.py
+++ Users/views.py
@@ -6,10 +6,12 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.status import HTTP_204_NO_CONTENT
 
-from knox.views import LoginView as KnoxLoginView
+from knox import views as knox_views
 
 from common.utils.drf.viewsets import RUViewSet
 from Users import serializers, controllers
+
+from realtime.common import jwt_utils
 
 
 def setup_serializer(view, request, **kwargs):
@@ -20,7 +22,7 @@ def setup_serializer(view, request, **kwargs):
     return serializer
 
 
-class LoginAPI(KnoxLoginView):
+class LoginAPI(knox_views.LoginView):
     authentication_classes = []
     permission_classes = [AllowAny]
     serializer_class = serializers.UserSerializer.Login
@@ -33,7 +35,7 @@ class LoginAPI(KnoxLoginView):
         return super(LoginAPI, self).post(request, format=None)
 
 
-class ResetPasswordAPI(KnoxLoginView):
+class ResetPasswordAPI(knox_views.LoginView):
     permission_classes = []
     serializer_class = serializers.UserSerializer.ResetPassword
 
@@ -66,7 +68,7 @@ class AskForOTPCodeAPI(GenericAPIView):
         return Response(status=HTTP_204_NO_CONTENT)
 
 
-class RegisterAPI(KnoxLoginView):
+class RegisterAPI(knox_views.LoginView):
     permission_classes = [AllowAny]
     authentication_classes = []
     serializer_class = serializers.UserSerializer.Register
@@ -92,3 +94,19 @@ class UserProfileAPI(RUViewSet):
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutAPI(knox_views.LogoutView):
+    def post(self, request, format=None):
+        response = super().post(request, format)
+        jwt_utils.JwtManager.remove_cookie(response)
+
+        return response
+
+
+class LogoutAllAPI(knox_views.LogoutAllView):
+    def post(self, request, format=None):
+        response = super().post(request, format)
+        jwt_utils.JwtManager.remove_cookie(response)
+
+        return response
